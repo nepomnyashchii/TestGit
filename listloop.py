4f95b331b16fe12062af8ba4f74152a5a02db93d
Alexander Yakovis <coolspammail@gmail.com>
2019-03-15 13:56:03 +0000

    rename

--- listloop.py
+++ listloop.py
@@ -1,12 +1,11 @@
-list1= [1,2,3,4,5]
-list2= ["one","two","three","four","five"]
-
+list1 = [1, 2, 3, 4, 5]
+list2 = ["one", "two", "three", "four", "five"]
 
 
 for element in list1:
-  print(element)
+    print(element)
 
-i=0 
-while i<len(list2):
-	print(list2[i])
-	i=i+1
+i = 0
+while i < len(list2):
+    print(list2[i])
+    i = i+1
