b32cb71722300e5bee27170cf66f2791e4ad66f8
Nepomnyashchii <nepomnyashchii@gmail.com>
2019-03-19 21:37:29 -0400

    first commit

---
+++ githubfileforloopwhile.py
@@ -0,0 +1,50 @@
+# word = "basketball"
+# for letter in word:
+#     print (letter)
+#
+# number = 120
+# while number > 112:
+#     print(number)
+#     number = number - 0.5
+#
+# print('My name is Alec')
+#
+#
+# i = 2
+# while i < 9:
+#     print(i)
+#     i = i+2
+#
+# numbers = list()
+# i = 3
+# while (i < 12):
+#     numbers.append(i)
+#     i = i+2
+# print(numbers)
+# for element in numbers:
+#     print(element)
+#
+# lines = list()
+# number = int(input('How many elephents do you have? '))
+# for i in range(number):
+#     elephant = input('The elephant: ')
+#     lines.append(elephant)
+# print("My favourite elephants are:")
+# for line in lines:
+#     print(line)
+
+# number = 21
+#
+# while number >= 0:
+#     print ('My favourite number is = ', number)
+#     number = number - 1
+#     if number == 17:
+#         break
+
+# while True:
+#     m = input("My favourite team: ")
+#     if not m:
+#         break
+#     if m.startswith("#"):
+#         continue
+#     print("Length", len(m))
