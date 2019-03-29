b7d27d2f533dea437d03d6d428bc0d09ba806a0b
nepomnyashchii <nepomnyashchii@gmail.com>
2019-03-29 16:23:56 +0000

    import ospractice

---
+++ ex3/ostrymore.py
@@ -0,0 +1,72 @@
+# import os
+# print (dir(os))
+
+# import os
+# print(os.getcwd())
+
+# import os
+# print(os.getcwd())
+# os.chdir('/Users/3/Desktop/')
+# print(os.getcwd())
+
+# import os
+# os.chdir('/Users/3/Desktop/')
+# print(os.listdir())
+
+# import os
+# os.chdir('/Users/3/Desktop/')
+# print(os.makedirs("z/c"))
+
+# import os
+# os.chdir('/Users/3/Desktop/')
+# print(os.rmdir("a"))
+
+# import os
+# os.chdir('/Users/3/Desktop/')
+# os.rename ("testos.txt", "test1os.txt")
+
+# import os
+# os.chdir('/Users/3/Desktop/')
+# print(os.stat("test1os.txt").st_size)
+
+# import os
+# from datetime import datetime
+# os.chdir('/Users/3/Desktop/')
+# mod_time= os.stat("test1os.txt").st_mtime
+# print(datetime.fromtimestamp(mod_time))
+
+# import os
+# from datetime import datetime
+# os.chdir('/Users/3/Desktop/')
+# for dirpath, dirnames, filenames in os.walk('/Users/3/Desktop/'):
+#     print("Current path: ", dirpath)
+#     print("Directories", dirnames)
+#     print("Files: ", filenames)
+#     print()
+
+
+# import os
+# from datetime import datetime
+# os.chdir("/Users/3/Desktop")
+# print(os.path.exists("/Users/3/Desktop"))
+
+# import os
+# from datetime import datetime
+# os.chdir("/Users/3/Desktop")
+# print(os.path.isdir("/Users/3/Desktop"))
+
+# import os
+# from datetime import datetime
+# os.chdir("/Users/3/Desktop")
+# print(os.path.isfile("/Users/3/Desktop"))
+
+
+# import os
+# from datetime import datetime
+# os.chdir("/Users/3/Desktop")
+# print(os.path.isfile("/Users/3/Desktop/test1os.txt"))
+
+# import os
+
+# os.chdir("/Users/3/Desktop")
+# print(dir(os.path))
