082c1ca50f71dd22a39df052608991f6981bfa27
nepomnyashchii <nepomnyashchii@gmail.com>
2019-04-16 21:18:37 +0000

    changes

---
+++ db/008.py
@@ -0,0 +1,16 @@
+import mysql.connector
+
+mydb = mysql.connector.connect(
+    host="db4free.net",
+    user="coolspammail",
+    passwd="coolspammail-pass",
+    database="coolspammail"
+    )
+mycursor = mydb.cursor()
+sql = "INSERT INTO customers (name, address) VALUES (%s, %s)"
+val = ("Michelle", "Blue Village")
+mycursor.execute(sql, val)
+
+mydb.commit()
+
+print("1 record inserted, ID:", mycursor.lastrowid)
