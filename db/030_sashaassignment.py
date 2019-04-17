2312619c0186e9e843bf2f76042d702c27dae607
nepomnyashchii <nepomnyashchii@gmail.com>
2019-04-17 22:47:20 +0000

    generation

--- db/030_sashaassignment.py
+++ db/030_sashaassignment.py
@@ -1,23 +1,23 @@
+import mysql.connector
+import uuid
 
-# import uuid
 def put_secret(msg: string, exp: int (seconds, pin int):
-# import mysql.connector
-    # uuid.uuid4()
-# mydb = mysql.connector.connect(
-#     host="db4free.net",
-#     user="coolspammail",
-#     passwd="coolspammail-pass",
-#     database="coolspammail"
-#     )
-# mycursor = mydb.cursor()
-# sql = "INSERT INTO secrets (put_secret) VALUES (%s, %s, %s)"
-# val = ("John", "Highway 21")
+    uuid.uuid4()
+mydb = mysql.connector.connect(
+    host="db4free.net",
+    user="coolspammail",
+    passwd="coolspammail-pass",
+    database="coolspammail"
+    )
+mycursor = mydb.cursor()
+sql = "INSERT INTO secrets (put_secret) VALUES (%s, %s, %s)"
+val = ("John", "Highway 21")
 
-# mycursor.execute(sql, val)
+mycursor.execute(sql, val)
 
-# mydb.commit()
+mydb.commit()
 
-# print(mycursor.rowcount, "record inserted.")
+print(mycursor.rowcount, "record inserted.")
 
 
 
