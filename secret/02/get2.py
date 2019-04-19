33d4d5505c87708c9b36c2a192d0541ff2e3075b
nepomnyashchii <nepomnyashchii@gmail.com>
2019-04-19 13:47:34 +0000

    changes

--- secret/02/get2.py
+++ secret/02/get2.py
@@ -1,35 +1,35 @@
-# import mysql.connector
-# print("Please wait...")
-# msg ="Privet"
-# try:
-#     mydb = mysql.connector.connect(
-#         host="db4free.net",
-#         user="coolspammail",
-#         passwd="coolspammail-pass",
-#         database="coolspammail"
-#     )
-#     mycursor = mydb.cursor()
+import mysql.connector
+print("Please wait...")
+msg ="Privet"
+try:
+    mydb = mysql.connector.connect(
+        host="db4free.net",
+        user="coolspammail",
+        passwd="coolspammail-pass",
+        database="coolspammail"
+    )
+    mycursor = mydb.cursor()
 
-#     sid = 'cc27a6dc-2e22-4a71-8a7f-4d104d2abad4'
-#     pin = 12345
-
-
-#     sql = "SELECT * FROM secret WHERE id =  %s AND pin = %s"
-#     params = (sid, int(pin))
-
-#     mycursor.execute(sql, params)
+    sid = 'cc27a6dc-2e22-4a71-8a7f-4d104d2abad4'
+    pin = 12345
 
 
-#     myresult = mycursor.fetchall()
+    sql = "SELECT * FROM secret WHERE id =  %s AND pin = %s"
+    params = (sid, int(pin))
 
-#     for x in myresult:
-#         print(x)
-#     msg ="Privet"
-#     return_value=msg
+    mycursor.execute(sql, params)
 
 
-# except:
-#     print ("Something went Wrong")
-# print('return_value:' + return_value)
+    myresult = mycursor.fetchall()
+
+    for x in myresult:
+        print(x)
+    msg ="Ura"
+    return_value=msg
+
+
+except:
+    print ("Something went Wrong")
+print('return_value:' + return_value)
 
 
