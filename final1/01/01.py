69e487ad2abc1eed98e0fb25c857b4afc5512c67
Alexander Yakovis <coolspammail@gmail.com>
2019-04-25 17:07:06 +0000

    fix

--- final1/01/01.py
+++ final1/01/01.py
@@ -5,7 +5,7 @@ import lib
 exp = 6000
 # # pin = 1234
 created = (2019, 4, 24, 12, 30, 11)
-finaltime =lib.time_expired (created, exp)
+finaltime = lib.time_expired(created, exp)
 # print(finaltime)
 # sid = lib.put_secret(msg, pin, exp)
 # msg = lib.get_secret(sid, pin)
--- final1/01/lib.py
+++ final1/01/lib.py
@@ -1,10 +1,10 @@
 import datetime
 def time_expired (created, exp):
-    time=datetime.datetime(2019, 4, 24, 12, 30, 11)
-    my_time = time + datetime.timedelta(0, exp)
+    my_time = created + datetime.timedelta(0, exp)
     current_time = datetime.datetime.now()
-    print(time, current_time, my_time)
+    print(created, current_time, my_time)
     if my_time > current_time:
-        print(True)
+        return True
     else:
-        print (False)
+        return False
+
