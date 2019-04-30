b2f2bbbed0c201b92bad3408aefaba85b171c9b4
nepomnyashchii <nepomnyashchii@gmail.com>
2019-04-30 02:13:05 +0000

    changes

--- flask2/001.py
+++ flask2/001.py
@@ -2,6 +2,6 @@ from flask import Flask
 import lib
 
 app = Flask(__name__)
-@app.route('/put/<int:ml>/')
+@app.route('/put/<int:ml>')
 def put(ml):
     return lib.oz_to_ml(ml)
--- flask2/lib.py
+++ flask2/lib.py
@@ -1,6 +1,6 @@
 
 def oz_to_ml(ml):
     oz= ml*29.97
-    return oz
+    return str(oz)
 # print(oz_to_ml())
 
