2bc8639018701983bc34e07e73b1f2bba4947b97
nepomnyashchii <nepomnyashchii@gmail.com>
2019-05-06 21:36:24 +0000

    responce

---
+++ sashaexercise/007.py
@@ -0,0 +1,18 @@
+response {
+    "scriptName": "goodmorning",
+    "description": "Dedicated server for web hosting",
+    "status": "active",
+    "created_time": "2019-04-05 17:38:06",
+    "data": {
+        "norris": [
+            "string1",
+            "string2",
+            "string3"
+        ],
+        "news": [
+        	"string1",
+            "string2",
+            "string3"
+        	]
+    }
+}
