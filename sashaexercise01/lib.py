a3de3ddf18f6879ee1121b90dd3b8d34014f902a
Alexander Yakovis <coolspammail@gmail.com>
2019-05-10 14:28:33 +0000

    fix

--- sashaexercise01/lib.py
+++ sashaexercise01/lib.py
@@ -1,27 +1,27 @@
 import mysql.connector
 
+
 def get_mixedtables(username, flow):
     myresult = ''
 
     mydb = mysql.connector.connect(
-    host="db4free.net",
-    user="coolspammail",
-    passwd="coolspammail-pass",
-    database="coolspammail"
+        host="db4free.net",
+        user="coolspammail",
+        passwd="coolspammail-pass",
+        database="coolspammail"
     )
     mycursor = mydb.cursor()
-    sql = " SELECT action ,action_order as a_order
+    sql = """SELECT action
             FROM `flows` INNER JOIN users ON flows.user_id = users.id
             WHERE LOWER(users.name)=%s AND flows.name =%s
-            ORDER BY action_order"
-
-    mycursor.execute(sql)
+            ORDER BY action_order
+            """
+    # NEW
+    val = (username, flow)
+    mycursor.execute(sql, val)
+    # old
+    # mycursor.execute(sql)
 
     myresult = mycursor.fetchall()
 
     return (myresult)
-
-
-
-
-
--- sashaexercise01/run.py
+++ sashaexercise01/run.py
@@ -3,9 +3,17 @@ import lib
 # print(__name__)
 app = Flask(__name__)
 
+
+@app.route('/')
+def index():
+    return 'Flow Run :)'
+
+
 @app.route('/run/<string:username>/<string:flow>')
 def run(username, flow):
     data = lib.get_mixedtables(username, flow)
-    return data (username, flow)
-
-
+    return jsonify(
+        data=data,
+        username=username,
+        flow=flow
+    )
