7d7f401aa2d17be599ab25a5796ea801c52266bc
nepomnyashchii <nepomnyashchii@gmail.com>
2019-05-10 20:27:44 +0000

    changes

--- sashaexercise01/lib.py
+++ sashaexercise01/lib.py
@@ -4,24 +4,61 @@ import mysql.connector
 def get_mixedtables(username, flow):
     myresult = ''
 
-    mydb = mysql.connector.connect(
-        host="db4free.net",
-        user="coolspammail",
-        passwd="coolspammail-pass",
-        database="coolspammail"
-    )
-    mycursor = mydb.cursor()
-    sql = """SELECT action
-            FROM `flows` INNER JOIN users ON flows.user_id = users.id
-            WHERE LOWER(users.name)=%s AND flows.name =%s
-            ORDER BY action_order
-            """
-    # NEW
-    val = (username, flow)
-    mycursor.execute(sql, val)
-    # old
-    # mycursor.execute(sql)
+    # mydb = mysql.connector.connect(
+    #     host="db4free.net",
+    #     user="coolspammail",
+    #     passwd="coolspammail-pass",
+    #     database="coolspammail"
+    # )
+    # mycursor = mydb.cursor()
+    # sql = """SELECT action
+    #         FROM `flows` INNER JOIN users ON flows.user_id = users.id
+    #         WHERE LOWER(users.name)=%s AND flows.name =%s
+    #         ORDER BY action_order
+    #         """
+    # # NEW
+    # val = (username, flow)
+    # mycursor.execute(sql, val)
+    # # old
+    # # mycursor.execute(sql)
 
-    myresult = mycursor.fetchall()
+    # myresult = mycursor.fetchall()
 
-    return (myresult)
+    # return (myresult)
+
+def run_action(actionline):
+    splited = actionline[0].split(":")
+    print(splited)
+    action = splited[0]
+    print(action)
+    # action = actionline[0].split(":")[0]
+    if action == 'news':
+        return get_news(actionline)
+    if action == 'norris':
+        return get_norris(actionline)
+    return {
+            "action": action,
+
+        }
+
+
+def get_news(actionline):
+    # splited = actionline[0].split(":")
+    # count = int(splited[1])
+    count= int(actionline[0].split(":")[1])
+    # fetch data from internet here
+    return {
+        "news": [],
+        "count": str(count)
+    }
+
+
+def get_norris(actionline):
+    splited = actionline[0].split(":")
+    count = int(splited[1])
+    # fetch data from internet here
+    return {
+        "norris": [],
+        "count": str(count)
+    }
+
--- sashaexercise01/run.py
+++ sashaexercise01/run.py
@@ -1,5 +1,6 @@
 from flask import Flask, request, json, jsonify
 import lib
+import datetime
 # print(__name__)
 app = Flask(__name__)
 
@@ -11,9 +12,22 @@ def index():
 
 @app.route('/run/<string:username>/<string:flow>')
 def run(username, flow):
-    data = lib.get_mixedtables(username, flow)
+#     # data = lib.get_mixedtables(username, flow)
+#     # return jsonify(
+#     #     data=data,
+#     #     username=username,
+#     #     flow=flow
+#     # )
+    data = [(u'news:3', 1), (u'norris:3', 2)]
+    print(data)
+    simpleList = []
+    for line in data:
+        action_data = lib.run_action(line)
+        simpleList.append(action_data)
+
     return jsonify(
-        data=data,
+        data=simpleList,
         username=username,
-        flow=flow
+        flow=flow,
+        time=datetime.datetime.now()
     )
