1aaa9085f420f9104804708e0e52234a82b30a08
Alexander Yakovis <coolspammail@gmail.com>
2019-05-13 20:17:55 +0000

    finish flows

--- flows/lib.py
+++ flows/lib.py
@@ -1,4 +1,6 @@
 import mysql.connector
+import requests
+import json
 
 
 def get_flowdata(username, flow):
@@ -42,29 +44,57 @@ def run_action(actionline):
     splited = actionline[0].split(":")
     action = splited[0]
     if action == 'news':
-        return get_news(actionline)
+        return run_news_action(actionline)
     if action == 'norris':
-        return get_norris(actionline)
+        return run_norris_action(actionline)
     return {
         "action": action,
     }
 
 
-def get_news(actionline):
+def run_news_action(actionline):
     splited = actionline[0].split(":")
     count = int(splited[1])
-    # fetch data from internet here
-    return {
-        "news": [],
-        "count": str(count)
-    }
+    return get_news(count)
 
-
-def get_norris(actionline):
+def run_norris_action(actionline):
     splited = actionline[0].split(":")
     count = int(splited[1])
-    # fetch data from internet here
-    return {
-        "norris": [],
-        "count": str(count)
-    }
+    return get_norris(count)
+
+
+
+def get_news(count):
+    news_data = requests.get(
+        "https://newsapi.org/v1/articles?pageSize=3&source=hacker-news&apiKey=c39a26d9c12f48dba2a5c00e35684ecc")
+    return_articles_list = convert_news(news_data, count)
+    return return_articles_list
+
+def get_norris(count):
+    norris_data = requests.get(
+        'http://api.icndb.com/jokes/random/' + str(count))
+    return_list = convert_norris(norris_data)
+    return return_list
+
+
+def convert_news(news_data, count):
+    news_obj = news_data.json()
+    source_articles = news_obj["articles"]
+    return_articles_list = []
+    for source_article in source_articles[:count]:
+        article = {
+            "title": source_article["title"],
+            "description": source_article["description"]
+        }
+        return_articles_list.append(article)
+
+    return return_articles_list
+
+
+def convert_norris(norris_data):
+    obj = norris_data.json()
+    source_list = obj["value"]
+    return_list = []
+    for source_item in source_list:
+        return_list.append(source_item["joke"])
+    return return_list
