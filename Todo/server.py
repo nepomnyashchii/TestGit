75c035fbf494b8e0266bc2bf4c0fbd1b9f54fc96
nepomnyashchiy <nepomnyashchii@utexas.edu>
2019-05-29 14:55:17 +0000

    all commited

---
+++ Todo/server.py
@@ -0,0 +1,22 @@
+from flask import Flask, jsonify
+import lib
+
+app = Flask(__name__)
+app.config['JSON_SORT_KEYS'] = False
+
+print("\n\n\n\n\n\n\n\n\n\n")
+
+
+@app.route('/')
+def index():
+    return 'Flow Runner :)'
+
+
+@app.route('/todo')
+def get_todo():
+    data = lib.get_todo()
+    print(data)
+    return data
+
+# data = lib.get_todo()
+# print(data)
