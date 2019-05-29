4074d1a81e8b7b7fe899b81784a51f9634139f68
Alexander Yakovis <coolspammail@gmail.com>
2019-05-29 20:31:43 +0000

    fix

--- Todo/lib.py
+++ Todo/lib.py
@@ -18,18 +18,22 @@ import json
 #     myresult = mycursor.fetchall()
 #     return myresult
 
+
 def get_todo(id):
     myresult = ''
     mydb = mysql.connector.connect(
-            host="db4free.net",
-            user="coolspammail",
-            passwd="coolspammail-pass",
-            database="coolspammail"
+        host="db4free.net",
+        user="coolspammail",
+        passwd="coolspammail-pass",
+        database="coolspammail"
     )
     mycursor = mydb.cursor()
-    sql = "SELECT * FROM `todo` WHERE id = %s"
-    val= (id)
-    mycursor.execute(sql,val)
-    myresult = mycursor.fetchall()
-    return myresult
+    # sql = "SELECT * FROM todo WHERE id = " + str(id) + ";"
+    # mycursor.execute(sql)
 
+    sql = "SELECT * FROM todo WHERE id = %s;"
+    val = (id,)
+    mycursor.execute(sql, val)
+
+    myresult = mycursor.fetchone()
+    return myresult
