be581b1283d94cb89a651898cef11802ae313ad8
nepomnyashchiy <nepomnyashchii@utexas.edu>
2019-05-29 19:51:08 +0000

    commit2

--- Todo/lib.py
+++ Todo/lib.py
@@ -19,18 +19,17 @@ import json
 #     return myresult
 
 def get_todo(id):
-    """get flowdata from db."""
     myresult = ''
     mydb = mysql.connector.connect(
             host="db4free.net",
             user="coolspammail",
             passwd="coolspammail-pass",
             database="coolspammail"
-        )
-
+    )
     mycursor = mydb.cursor()
-    sql = "SELECT * FROM `todo` WHERE id=%s"
-    val = (id)
+    sql = "SELECT * FROM `todo` WHERE id = %s"
+    val= (id)
     mycursor.execute(sql,val)
     myresult = mycursor.fetchall()
     return myresult
+
