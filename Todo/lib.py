6d547c25ebc0dd735cf4e7d9da508cbd75248ea9
nepomnyashchiy <nepomnyashchii@utexas.edu>
2019-05-29 19:40:43 +0000

    all commitment

--- Todo/lib.py
+++ Todo/lib.py
@@ -2,7 +2,23 @@ import mysql.connector
 import requests
 import json
 
-def get_todo():
+# def get_todo():
+#     """get flowdata from db."""
+#     myresult = ''
+#     mydb = mysql.connector.connect(
+#             host="db4free.net",
+#             user="coolspammail",
+#             passwd="coolspammail-pass",
+#             database="coolspammail"
+#         )
+
+#     mycursor = mydb.cursor()
+#     sql = " SELECT * FROM `todo`"
+#     mycursor.execute(sql)
+#     myresult = mycursor.fetchall()
+#     return myresult
+
+def get_todo(id):
     """get flowdata from db."""
     myresult = ''
     mydb = mysql.connector.connect(
@@ -13,7 +29,8 @@ def get_todo():
         )
 
     mycursor = mydb.cursor()
-    sql = " SELECT * FROM `todo`"
-    mycursor.execute(sql)
+    sql = "SELECT * FROM `todo` WHERE id=%s"
+    val = (id)
+    mycursor.execute(sql,val)
     myresult = mycursor.fetchall()
     return myresult
--- Todo/server.py
+++ Todo/server.py
@@ -12,12 +12,17 @@ def index():
     return 'Flow Runner :)'
 
 
-@app.route('/todo')
-def get_todo():
-    data = lib.get_todo()
+# @app.route('/todo')
+# def get_todo():
+#     data = lib.get_todo()
+#     print(data)
+#     return str(data)
+
+@app.route('/todo/<int:id>')
+def get_todo(id):
+    data = lib.get_todo(id)
     print(data)
     return str(data)
-
 
 
 # data = lib.get_todo()
