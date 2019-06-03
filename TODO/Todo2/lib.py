3bfffca51e768b4a9f9957bc29689bcc7b589ab9
nepomnyashchiy <nepomnyashchii@utexas.edu>
2019-06-03 19:37:14 +0000

    allchanges

--- TODO/Todo2/lib.py
+++ TODO/Todo2/lib.py
@@ -10,35 +10,33 @@ mydb = mysql.connector.connect(
 
 def insert_furniture(chairs, tables):
     mycursor = mydb.cursor()
-    sql = "INSERT INTO `todo` (id, `chairs`,`tables`) VALUES (%s,%s,%s)"
+    sql = "INSERT INTO `furniture` (id, `chairs`,`tables`) VALUES (%s,%s,%s)"
     val = (id, chairs, tables)
     mycursor.execute(sql, val)
     mydb.commit()
     return mycursor.lastrowid
 
-def get_everything():
-    """get flowdata from db."""
-    myresult = ''
-    mycursor = mydb.cursor()
-    sql = "SELECT * FROM `furniture`"
-    mycursor.execute(sql)
-    myresult = mycursor.fetchall()
-    return myresult
+# def get_everything():
+#     mycursor = mydb.cursor()
+#     sql = "SELECT * FROM `furniture`"
+#     mycursor.execute(sql)
+#     myresult = mycursor.fetchall()
+#     return myresult
 
 
-def get_furniture_by_id(id):
-    myresult = ''
-    mycursor = mydb.cursor()
-    sql = "SELECT * FROM todo WHERE id = %s;"
-    val = (id,)
-    mycursor.execute(sql, val)
-    myresult = mycursor.fetchone()
-    return myresult
+# def get_furniture_by_id(id):
+#     myresult = ''
+#     mycursor = mydb.cursor()
+#     sql = "SELECT * FROM todo WHERE id = %s;"
+#     val = (id,)
+#     mycursor.execute(sql, val)
+#     myresult = mycursor.fetchone()
+#     return myresult
 
-def delete_todo_by_id(id):
-    mycursor = mydb.cursor()
-    sql = "DELETE FROM todo where id= %s"
-    val =(id,)
-    mycursor.execute(sql, val)
-    mydb.commit()
-    return mycursor.rowcount
+# def delete_todo_by_id(id):
+#     mycursor = mydb.cursor()
+#     sql = "DELETE FROM todo where id= %s"
+#     val =(id,)
+#     mycursor.execute(sql, val)
+#     mydb.commit()
+#     return mycursor.rowcount
--- TODO/Todo2/run.py
+++ TODO/Todo2/run.py
@@ -1,18 +1,19 @@
 import lib
+
 chairs= "italian"
 tables = "french"
 
 designated_id = lib.insert_furniture(chairs, tables)
 print(designated_id)
 
-everything_data = lib.get_everything()
-print(everything_data)
+# everything_data = lib.get_everything()
+# print(everything_data)
 
-identification = lib.get_furniture_by_id(designated_id)
-print(identification)
+# identification = lib.get_furniture_by_id(designated_id)
+# print(identification)
 
 # updated = lib.update_todo_by_id(designated_id, "Spanish", "Russian")
 # print(updated)
 
-response = lib.delete_todo_by_id(designated_id-1)
-print(response)
+# response = lib.delete_todo_by_id(designated_id-1)
+# print(response)
