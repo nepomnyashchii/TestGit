79bfffdc3f6ad5bc5fcb864805033dce4122998f
Alexander Yakovis <coolspammail@gmail.com>
2019-06-04 14:49:04 +0000

    alex-fix

--- TODO/Todo2/lib.py
+++ TODO/Todo2/lib.py
@@ -16,27 +16,27 @@ def insert_furniture(chairs, tables):
     mydb.commit()
     return mycursor.lastrowid
 
-# def get_everything():
-#     mycursor = mydb.cursor()
-#     sql = "SELECT * FROM `furniture`"
-#     mycursor.execute(sql)
-#     myresult = mycursor.fetchall()
-#     return myresult
+def get_everything():
+    mycursor = mydb.cursor()
+    sql = "SELECT * FROM `furniture`"
+    mycursor.execute(sql)
+    myresult = mycursor.fetchall()
+    return myresult
 
 
-# def get_furniture_by_id(id):
-#     myresult = ''
-#     mycursor = mydb.cursor()
-#     sql = "SELECT * FROM todo WHERE id = %s;"
-#     val = (id,)
-#     mycursor.execute(sql, val)
-#     myresult = mycursor.fetchone()
-#     return myresult
+def get_furniture_by_id(id):
+    myresult = ''
+    mycursor = mydb.cursor()
+    sql = "SELECT * FROM furniture WHERE id = %s;"
+    val = (id,)
+    mycursor.execute(sql, val)
+    myresult = mycursor.fetchone()
+    return myresult
 
-# def delete_todo_by_id(id):
-#     mycursor = mydb.cursor()
-#     sql = "DELETE FROM todo where id= %s"
-#     val =(id,)
-#     mycursor.execute(sql, val)
-#     mydb.commit()
-#     return mycursor.rowcount
+def delete_todo_by_id(id):
+    mycursor = mydb.cursor()
+    sql = "DELETE FROM furniture where id= %s"
+    val =(id,)
+    mycursor.execute(sql, val)
+    mydb.commit()
+    return mycursor.rowcount
