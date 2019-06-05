a7f130748f639d1590c4d8d091e645c9c1ed15c1
nepomnyashchiy <nepomnyashchii@utexas.edu>
2019-06-05 18:59:40 +0000

    allchanges

--- TODO/Todo2/run.py
+++ TODO/Todo2/run.py
@@ -1,59 +1,17 @@
-
-from flask import Flask, request, jsonify
 import lib
 
-app = Flask(__name__)
-app.config['JSON_SORT_KEYS'] = False
 
-print("\n\n\n\n\n\n\n\n\n\n")
+new_id = lib.insert_todo("elephant")
+print(new_id)
 
+all_data = lib.get_all()
+print(all_data)
 
-@app.route('/')
-def index():
-    return 'Flow Runner :)'
-# get data through todo (not forget to transfer obtained tupple to the string)
+new_id_data = lib.get_todo_by_id(new_id)
+print(new_id)
 
-@app.route('/todo')
-def get_todo():
-    all_data = lib.get_all()
-    print(all_data)
-    return str(all_data)
+updated = lib.update_todo_by_id(new_id, "monkey", 100)
+print(updated)
 
-@app.route('/todo/<int:id>')
-def get_all(id):
-    id_data = lib.get_todo_by_id(id)
-    print(id_data)
-    return str(id_data)
-
-@app.route('/todo', methods = ['POST'])
-def insert_todo():
-    data = request.json
-    # jsonify(
-    #     data=data,
-    # )
-    new_id = lib.insert_todo(data)
-    print(new_id)
-    return new_id
-    # return jsonify(
-    #     data=data,
-    # )
-@app.route('/todo/<int:id>', methods = ['PUT'])
-def update_delete_todo(id):
-    updated = lib.update_todo_by_id(id, "monkey", 100)
-    print(updated)
-    data = request.json
-    # delete_previous_id = lib.delete_todo_by_id(id-1)
-    # print (delete_previous_id)
-    print(data)
-    return jsonify(
-        data=data,
-    )
-
-
-# {
-#     text:"asdasd",
-#     done:true
-# }
-
-
-
+response = lib.delete_todo_by_id(new_id-1)
+print(response)
