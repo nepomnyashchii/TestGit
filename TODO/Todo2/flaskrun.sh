420cf0ce914696f4865b7e0cb5d168c4ddf70fdc
nepomnyashchiy <nepomnyashchii@utexas.edu>
2019-06-05 18:12:15 +0000

    allchanges

---
+++ TODO/Todo2/flaskrun.sh
@@ -0,0 +1,3 @@
+
+FLASK_APP=run.py flask run
+find . -name '*.pyc' -delete
--- TODO/Todo2/run.py
+++ TODO/Todo2/run.py
@@ -1,19 +1,56 @@
+
+from flask import Flask, request, jsonify
 import lib
 
-chairs= "italian"
-tables = "french"
+app = Flask(__name__)
+app.config['JSON_SORT_KEYS'] = False
 
-designated_id = lib.insert_furniture(chairs, tables)
-print(designated_id)
+print("\n\n\n\n\n\n\n\n\n\n")
 
-everything_data = lib.get_everything()
-print(everything_data)
 
-identification = lib.get_furniture_by_id(designated_id)
-print(identification)
+@app.route('/')
+def index():
+    return 'Flow Runner :)'
+# get data through todo (not forget to transfer obtained tupple to the string)
 
-# updated = lib.update_todo_by_id(designated_id, "Spanish", "Russian")
-# print(updated)
+@app.route('/todo')
+def get_todo():
+    all_data = lib.get_all()
+    print(all_data)
+    return str(all_data)
 
-response = lib.delete_todo_by_id(designated_id-1)
-print(response)
+@app.route('/todo/<int:id>')
+def get_all(id):
+    id_data = lib.get_todo_by_id(id)
+    print(id_data)
+    return str(id_data)
+
+@app.route('/todo', methods = ['POST'])
+def insert_todo():
+    new_id = lib.insert_todo("jopa")
+    print(new_id)
+    data = request.json
+    print(data)
+    return jsonify(
+        data=data,
+    )
+@app.route('/<int:id>', methods = ['PUT'])
+def update_delete_todo(id):
+    updated = lib.update_todo_by_id(id, "FFF", 100)
+    print(updated)
+    data = request.json
+    # delete_previous_id = lib.delete_todo_by_id(id-1)
+    # print (delete_previous_id)
+    print(data)
+    return jsonify(
+        data=data,
+    )
+
+
+# {
+#     text:"asdasd",
+#     done:true
+# }
+
+
+
