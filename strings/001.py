ce68541eacc9c366f9a1bf68d82a559e8a222554
nepomnyashchii <nepomnyashchii@gmail.com>
2019-06-06 23:17:13 +0000

    all changes

--- strings/001.py
+++ strings/001.py
@@ -1,18 +1,20 @@
 print("I bought %d Euro worth of %s!" %(200, 'cookies'))
 print("I bought 200 Euro worth of cookies!")
 lecture ="jopa"
-"text" = '" + lecture +  "'
-print(lake)
+print(lecture)
 print("abc"+str(9))
 print("abs{0}".format(9))
-count =5
+print("nba{:2f}".format(9.1756))
+print "{} {} {} {} {}".format("hello", "world", 25, [1,1,2,3], (1,2,3))
+print("{} {} {} {} {} {}").format(1,2,3,4,5,6)
+count ="9"
 print(int(count) +9)
+print("My news" + str(count))
 print("abq%s" % 9)
 # "abq%s" % 9 = "abq" +str(9)
 hello = "hello"
 world = "world"
-
-print (hello + " " + world)
+print (hello + "    " + world)
 print ("%s %s" % (hello, world))
 print "{}, {}".format(hello, world)
 print ' '.join([hello, world])
