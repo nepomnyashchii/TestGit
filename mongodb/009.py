38471a94a6af2e9ff0e1fd733e2bab9e40242886
nepomnyashchii <nepomnyashchii@gmail.com>
2019-06-27 23:52:18 +0000

    ffff

--- mongodb/009.py
+++
@@ -1,9 +0,0 @@
-import pymongo
-import dns
-client = pymongo.MongoClient("mongodb+srv://nepomnyashchii:natasha1977#5@cluster0-6p7nv.mongodb.net/test?retryWrites=true&w=majority")
-db = client.test
-
-db.inventory.update_many(
-    {"item": "paper"},
-    {"$set": {"size.uom": "cm", "status": "P"},
-     "$currentDate": {"lastModified": True}})
--- mongodb/025.py
+++
@@ -1,9 +0,0 @@
-import pymongo
-import dns
-client = pymongo.MongoClient("mongodb+srv://nepomnyashchii:natasha1977#5@cluster0-6p7nv.mongodb.net/test?retryWrites=true&w=majority")
-db = client["test"]
-mycol = db["customers"]
-
-x = mycol.delete_many({})
-
-print(x.deleted_count, " documents deleted.")
