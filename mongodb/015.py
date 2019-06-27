586f9854918c012dd8ecb52e83ea1976893930e7
nepomnyashchii <nepomnyashchii@gmail.com>
2019-06-27 23:55:03 +0000

    vvv

--- mongodb/015.py
+++
@@ -1,8 +0,0 @@
-import pymongo
-import dns
-client = pymongo.MongoClient("mongodb+srv://nepomnyashchii:natasha1977#5@cluster0-6p7nv.mongodb.net/test?retryWrites=true&w=majority")
-db = client["test"]
-mycol = db["customers"]
-x = mycol.find_one()
-
-print(x)
--- mongodb/026.py
+++
@@ -1,6 +0,0 @@
-import pymongo
-import dns
-client = pymongo.MongoClient("mongodb+srv://nepomnyashchii:natasha1977#5@cluster0-6p7nv.mongodb.net/test?retryWrites=true&w=majority")
-db = client["test"]
-mycol = db["customers"]
-mycol.drop()
