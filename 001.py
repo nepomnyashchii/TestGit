fe604df7e7d23bceee11464f6c9ed5a01f2d6fdf
nepomnyashchii <nepomnyashchii@gmail.com>
2019-06-28 17:26:46 +0000

    gggg

---
+++ 001.py
@@ -0,0 +1,45 @@
+
+
+# def my_cubef(num):
+#     name = num*num*num
+#     print(name)
+#     return name
+# my_cubef(3)
+
+experiment = "cube"
+
+def my_day(jane):
+    elephant  = name = "my lab " + jane
+    if len(my_cube(experiment)) >0:
+        print(elephant)
+        print ("world is great")
+    else:
+        print("Data does not exist")
+    return elephant
+
+def my_cube(experiment):
+    name = "my lab is " + experiment
+    print(name)
+    return name
+
+my_day ("apple")
+
+
+
+# def my_cube(num):
+#     return num*num*num
+
+# print(my_cube(3))
+
+
+
+# def raise_to_power(base_num, pow_num):
+#     result = 1
+#     for element in range(pow_num):
+#         result = result*base_num
+#     return result
+
+
+# print(raise_to_power(3, 4))
+
+
