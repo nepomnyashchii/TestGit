07cac1434048506902af3c04f7607d7ce0fb06a8
Alexander Yakovis <coolspammail@gmail.com>
2019-07-01 21:09:34 +0000

    fix connection example

--- TODO/Todo6/lib.py
+++ TODO/Todo6/lib.py
@@ -1,33 +1,43 @@
+import json
 import mysql.connector
 import logger_module
 logger = logger_module.setup_logger("todo-2lib")
-import json
 
 
+def open_db():
+    mydb = mysql.connector.connect(
+        host="db4free.net",
+        user="coolspammail",
+        passwd="coolspammail-pass",
+        database="coolspammail"
+    )
+    return mydb
 
-mydb = mysql.connector.connect(
-    host="db4free.net",
-    user="coolspammail",
-    passwd="coolspammail-pass",
-    database="coolspammail"
-)
+
+def close_db(mydb):
+    mydb.close()
+
 
 def get_all():
     """get flowdata from db."""
+
     try:
+        mydb = open_db()
         mycursor = mydb.cursor()
         sql = "SELECT * FROM `todo`"
         mycursor.execute(sql)
-        row_headers=[x[0] for x in mycursor.description] #this will extract row headers
+        # this will extract row headers
+        row_headers = [x[0] for x in mycursor.description]
         rv = mycursor.fetchall()
-        json_data=[]
+        json_data = []
         for result in rv:
-            json_data.append(dict(zip(row_headers,result)))
+            json_data.append(dict(zip(row_headers, result)))
+        close_db(mydb)
         return json.dumps(json_data)
 
-    except: print("Something went wrong in get_all")
+    except:
+        print("Something went wrong in get_all")
     return json_data
-
 
 
 def get_todo_by_id(id):
@@ -37,16 +47,19 @@ def get_todo_by_id(id):
         sql = "SELECT * FROM todo WHERE id = %s;"
         val = (id,)
         mycursor.execute(sql, val)
-        row_headers=[x[0] for x in mycursor.description] #this will extract row headers
+        # this will extract row headers
+        row_headers = [x[0] for x in mycursor.description]
         rv = mycursor.fetchall()
-        json_data=[]
+        json_data = []
         for result in rv:
-            json_data.append(dict(zip(row_headers,result)))
+            json_data.append(dict(zip(row_headers, result)))
             return json.dumps(json_data)
-    except: print("Something went wrong with get_to_do_by_id")
-    if "done" == 0 or "done"==1:
+    except:
+        print("Something went wrong with get_to_do_by_id")
+    if "done" == 0 or "done" == 1:
         return json_data
-    else:return ("No real data exists")
+    else:
+        return ("No real data exists")
 
 
 def insert_todo(text):
@@ -57,8 +70,8 @@ def insert_todo(text):
         mycursor.execute(sql, val)
         mydb.commit()
         return mycursor.lastrowid
-    except: print("Something went wrong with insert_todo")
-
+    except:
+        print("Something went wrong with insert_todo")
 
 
 def delete_todo_by_id(id):
@@ -66,10 +79,11 @@ def delete_todo_by_id(id):
         mycursor = mydb.cursor()
         sql = "DELETE FROM todo where id=%s"
         val = (id,)
-        mycursor.execute(sql,val)
+        mycursor.execute(sql, val)
         mydb.commit()
         return mycursor.rowcount
-    except:print("Something went wrong with delete_todo_by_id")
+    except:
+        print("Something went wrong with delete_todo_by_id")
 
 
 def update_todo_by_id(id, text, done):
@@ -81,5 +95,5 @@ def update_todo_by_id(id, text, done):
         mydb.commit()
         # print(mycursor.rowcount, "record(s) affected")
         return id
-    except:  print("Something went wrong with updata_todo_by_id")
-
+    except:
+        print("Something went wrong with updata_todo_by_id")
