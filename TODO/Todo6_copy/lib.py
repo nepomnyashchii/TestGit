202231b1add81ac183d750b37730041f807713ee
nepomnyashchii <nepomnyashchii@gmail.com>
2019-07-02 12:52:08 +0000

    dddd

--- TODO/Todo6_copy/lib.py
+++ TODO/Todo6_copy/lib.py
@@ -13,95 +13,157 @@ def open_db():
     )
     return mydb
 
-
-def close_db(mydb):
-    mydb.close()
-
-
 def get_all():
     """get flowdata from db."""
 
     try:
+        logger.debug('Invoke def open_db()')
         mydb = open_db()
+        logger.debug("Start db")
         mycursor = mydb.cursor()
+        logger.debug("Start executing action for db")
         sql = "SELECT * FROM `todo`"
         mycursor.execute(sql)
         # this will extract row headers
-        row_headers = [x[0] for x in mycursor.description]
+        logger.debug("Collect list of tuples for all ids")
         rv = mycursor.fetchall()
+        logger.debug("All obtained data from database" +str(rv))
+        row_headers = [x[0] for x in mycursor.description]
         json_data = []
         for result in rv:
             json_data.append(dict(zip(row_headers, result)))
-        close_db(mydb)
-        return json.dumps(json_data)
-
+        logger.debug("Create dictionary with inserted row_headers from tuple" +str(json_data))
+        logger.debug("Close the database")
+        mydb.close()
+    except IOError:
+        logger.error('An error occured trying to read the file.')
+    except ValueError:
+        logger.error('Non-numeric data found in the file.')
+    except ImportError:
+        logger.error("NO module found")
+    except EOFError:
+        logger.error('Why did you do an EOF on me?')
+    except KeyboardInterrupt:
+        logger.error('You cancelled the operation.')
     except:
-        print("Something went wrong in get_all")
+        logger.debug('An error occured.')
     return json_data
 
 
 def get_todo_by_id(id):
     # myresult = ''
     try:
+        logger.debug('Invoke def open_db()')
         mydb = open_db()
+        logger.debug("Start db")
         mycursor = mydb.cursor()
         sql = "SELECT * FROM todo WHERE id = %s;"
         val = (id,)
         mycursor.execute(sql, val)
         # this will extract row headers
-        row_headers = [x[0] for x in mycursor.description]
+        logger.debug("Collect list of tuples for all ids")
         rv = mycursor.fetchall()
+        logger.debug("All obtained data from database" +str(rv))
+        row_headers = [x[0] for x in mycursor.description]
         json_data = []
         for result in rv:
             json_data.append(dict(zip(row_headers, result)))
-        close_db(mydb)
-    except:
-        print("Something went wrong with get_to_do_by_id")
-    if "done" == 0 or "done" == 1:
-        return json.dumps(json_data)
+        logger.debug("Create dictionary with inserted row_headers from tuples" +str(json_data))
+        logger.debug("Close the database")
+        mydb.close()
         return json_data
-    else:
-        return ("No real data exists")
+    except IOError:
+        logger.error('An error occured trying to read the file.')
+    except ValueError:
+        logger.error('Non-numeric data found in the file.')
+    except ImportError:
+        logger.error("NO module found")
+    except EOFError:
+        logger.error('Why did you do an EOF on me?')
+    except KeyboardInterrupt:
+        logger.error('You cancelled the operation.')
+    except:
+        logger.debug('An error occured.')
 
 
 def insert_todo(text):
     try:
+        logger.debug('Invoke def open_db()')
         mydb = open_db()
+        logger.debug("Start db")
         mycursor = mydb.cursor()
         sql = "INSERT INTO `todo` (`text`,`done`) VALUES (%s,%s)"
         val = (text, 0)
         mycursor.execute(sql, val)
+        logger.debug("Commit changes to the database")
         mydb.commit()
-        close_db(mydb)
+        logger.debug("Close the database")
+        mydb.close()
         return mycursor.lastrowid
+    except IOError:
+        logger.error('An error occured trying to read the file.')
+    except ValueError:
+        logger.error('Non-numeric data found in the file.')
+    except ImportError:
+        logger.error("NO module found")
+    except EOFError:
+        logger.error('Why did you do an EOF on me?')
+    except KeyboardInterrupt:
+        logger.error('You cancelled the operation.')
     except:
-        print("Something went wrong with insert_todo")
-
+        logger.debug('An error occured.')
 
 def delete_todo_by_id(id):
     try:
+        logger.debug('Invoke def open_db()')
         mydb = open_db()
+        logger.debug("Start db")
         mycursor = mydb.cursor()
         sql = "DELETE FROM todo where id=%s"
         val = (id,)
         mycursor.execute(sql, val)
+        logger.debug("Commit changes to the database")
         mydb.commit()
-        close_db(mydb)
+        logger.debug("Close the database")
+        mydb.close()
         return mycursor.rowcount
+    except IOError:
+        logger.error('An error occured trying to read the file.')
+    except ValueError:
+        logger.error('Non-numeric data found in the file.')
+    except ImportError:
+        logger.error("NO module found")
+    except EOFError:
+        logger.error('Why did you do an EOF on me?')
+    except KeyboardInterrupt:
+        logger.error('You cancelled the operation.')
     except:
-        print("Something went wrong with delete_todo_by_id")
-
+        logger.debug('An error occured.')
 
 def update_todo_by_id(id, text, done):
     try:
+        logger.debug('Invoke def open_db()')
         mydb = open_db()
+        logger.debug("Start db")
         mycursor = mydb.cursor()
         sql = "UPDATE todo SET `text` = %s, `done` = %s  WHERE id=%s"
         val = (text, done, id)
         mycursor.execute(sql, val)
+        logger.debug("Commit changes to the database")
         mydb.commit()
-        close_db(mydb)
+        logger.debug("Close the database")
+        mydb.close()
         # print(mycursor.rowcount, "record(s) affected")
         return id
+    except IOError:
+        logger.error('An error occured trying to read the file.')
+    except ValueError:
+        logger.error('Non-numeric data found in the file.')
+    except ImportError:
+        logger.error("NO module found")
+    except EOFError:
+        logger.error('Why did you do an EOF on me?')
+    except KeyboardInterrupt:
+        logger.error('You cancelled the operation.')
     except:
-        print("Something went wrong with updata_todo_by_id")
+        logger.debug('An error occured.')
