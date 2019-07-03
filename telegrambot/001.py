74947233b8393845f81435b64f779ac10d04324f
Alexander Yakovis <coolspammail@gmail.com>
2019-07-03 16:59:20 +0000

    fix

--- telegrambot/001.py
+++ telegrambot/001.py
@@ -1,10 +1,11 @@
 import telebot
 
-bot =telebot.TeleBot("873802609:AAGrF4tID-CD4lufzgMjqNEnqLxhCaEweLM")
+bot = telebot.TeleBot("873802609:AAGrF4tID-CD4lufzgMjqNEnqLxhCaEweLM")
 
 
 @bot.message_handler(content_types=['text'])
 def send_echo(message):
-    bot.send_message(message.chat.id, message.text)
+    bot.send_message(message.chat.id, message.text + '?')
 
-bot.polling (none_stop = True)
+
+bot.polling(none_stop=True)
