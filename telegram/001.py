6476d8b78e3f549537e84720d44c54ce71cadf4a
nepomnyashchii <nepomnyashchii@gmail.com>
2019-07-03 18:29:43 +0000

    fix

--- telegram/001.py
+++ telegram/001.py
@@ -5,6 +5,18 @@ import lib
 bot = telebot.TeleBot("873802609:AAGrF4tID-CD4lufzgMjqNEnqLxhCaEweLM")
 
 
+def generate_todo_text(data):
+    text = ''
+    for todo in data:
+        text += todo["text"]
+        if todo["done"] != 0:
+            text += ' - Done\n'
+        else:
+            text += '\n'
+    print(text)
+    return text
+
+
 @bot.message_handler(content_types=['text'])
 def send_echo(message):
     user = message.from_user
@@ -12,20 +24,14 @@ def send_echo(message):
     user_id = user.id
     user_fullname = user.first_name + ' ' + user.last_name
     print(user_name, user_id, user_fullname, message.text)
-
     if (message.text.lower() == 'todo'):
         data = lib.get_all()
-        text = ''
-        for todo in data:
-            text += todo["text"]
-            if todo["done"] != 0:
-                text += ' - Done\n'
-            else:
-                text += '\n'
-        print(text)
+        text = generate_todo_text(data)
         bot.send_message(message.chat.id, text)
-    elif (message.text == 'Flow'):
-        bot.send_message(message.chat.id, "What Flow u want to run")
+    elif (message.text.lower() == 'good morning'):
+        bot.send_message(
+            message.chat.id,
+            "Good morning flow for " + str(user_id))
     else:
         bot.send_message(message.chat.id, message.text)
 
