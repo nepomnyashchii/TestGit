b5e6595df2e6946cf50dbae7f6c092e4d390ebec
nepomnyashchii <nepomnyashchii@gmail.com>
2019-07-04 13:57:58 +0000

    sss

---
+++ training/dic.py
@@ -0,0 +1,27 @@
+
+"""SIMPLE DICTIONARY OPERATIONS"""
+
+"""Initial dictionary"""
+
+dic={"name" : "alec", "letter" : "apple"}
+print(dic)
+
+"""Call the parameter from dictionary"""
+result = dic["name"]
+print(result)
+
+"""Delete parameter from the dictionary"""
+dic.pop("name")
+print(dic)
+
+""" Add parameter to the dictionary"""
+dic["name"] ="John"
+print(dic)
+
+""" Change parameter in the dictionary"""
+dic["name"] = "Alex"
+print(dic)
+
+
+
+
