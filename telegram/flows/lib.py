612624a770a316efa2901825687694ebb84d1cdc
nepomnyashchii <nepomnyashchii@gmail.com>
2019-07-05 21:46:15 +0000

    ggg

--- telegram/flows/lib.py
+++ telegram/flows/lib.py
@@ -34,7 +34,7 @@ def get_nice_text_from_flows(data):
         if actionTitle == "weather":
             text += '==== Weather ====\n\n\n'
             result = action_data["temperature"]
-            result1 = result-273
+            result1 = result-273.00
             result2 = "Temperature: " + str(result) + " K"
             text += json.dumps(result1)
             text += '\n\n\n\n'
