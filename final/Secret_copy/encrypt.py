1466402c6d67dfac9c129512ba02d41bbb6920c9
nepomnyashchiy <nepomnyashchii@utexas.edu>
2019-07-09 22:22:37 +0000

    dddd

--- final/Secret_copy/encrypt.py
+++
@@ -1,6 +0,0 @@
-from cryptography.fernet import Fernet
-key = 'IscdSPJ0zku3uRTU9vqVvjQ3ekbg4_xfDbxcK8VvQAg='
-# data = Fernet(key)
-# # encrypted_text = data.encrypt(b'SuperSecretpassword')
-with open('1.txt', 'a') as f:
-    f.write(key)
---
+++ final/Secret_copy/generate_key.py
@@ -0,0 +1,4 @@
+from cryptography.fernet import Fernet
+key = 'IscdSPJ0zku3uRTU9vqVvjQ3ekbg4_xfDbxcK8VvQAg='
+with open('key.txt', 'a') as f:
+    f.write(key)
