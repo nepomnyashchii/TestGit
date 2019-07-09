1d9296929ff420bea535f3d834db47ba336341b8
nepomnyashchiy <nepomnyashchii@utexas.edu>
2019-07-09 20:06:13 +0000

    ww

--- final/Secret_copy/encrypt.py
+++ final/Secret_copy/encrypt.py
@@ -3,5 +3,5 @@ from cryptography.fernet import Fernet
 key = b'pRmgMa8T0INjEAfksaq2aafzoZXEuwKI7wDe4c1F8AY='
 # data = Fernet(key)
 # # encrypted_text = data.encrypt(b'SuperSecretpassword')
-with open('1.txt', 'w') as f:
+with open('1.txt', 'a') as f:
     f.write(key)
