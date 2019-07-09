37e9b7cdfb101e593411cced48abb62ce3367719
nepomnyashchiy <nepomnyashchii@utexas.edu>
2019-07-09 16:10:17 +0000

    jjj

---
+++ training/error.py
@@ -0,0 +1,24 @@
+
+# """ Syntax Error: EOL while scanning string literal"""
+# # print "hello
+
+
+# """Error out of range index """
+# list = [1,2,3]
+# print(list[3])
+
+# """NameError: name json is not defined"""
+# # alec = "mama"
+# # json.dumps(alec)
+
+""" TypeError: All integers or Strings"""
+# apple = 2 + "2"
+# print(apple)
+
+# "NameError: Name lake is not defined"
+# lake = int(apple)
+# print(lake)
+
+# """Syntax Error: InvalidSyntax"""
+# lake = int(aplple
+# print(lake)
