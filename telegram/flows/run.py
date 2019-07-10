bbb34a8648bb82121a9a73a1684e4accf0fe489a
nepomnyashchiy <nepomnyashchii@utexas.edu>
2019-07-10 22:43:45 +0000

    dddd

--- telegram/flows/run.py
+++ telegram/flows/run.py
@@ -1,49 +1,39 @@
 import telebot
 import json
 import lib
+import logger_module
+logger = logger_module.setup_logger("flowsapp")
 
+logger.debug("Start Telebot")
 bot = telebot.TeleBot("873802609:AAGrF4tID-CD4lufzgMjqNEnqLxhCaEweLM")
 
 
 @bot.message_handler(content_types=['text'])
-def send_echo(message):
+def send_userinformation(message):
+    """ print(user_name, user_id, user_fullname, message.text"""
     user = message.from_user
     user_name = user.username
     user_id = user.id
     user_fullname = user.first_name + ' ' + user.last_name
-    # print(user_name, user_id, user_fullname, message.text)
+    # logger.debug("user + user_name + user_id +user_fullname ") + user + " " + user_name + " " + user_id + user_fullname
+
 
     if (message.text.lower() == 'goodmorning'):
         person = "Alex"
+        logger.debug("Start flowdata")
         data = lib.get_flowdata(person, message.text.lower())
+        logger.debug("Obtain flowdata")
         content = lib.get_nice_text_from_flows(data)
-        # print(content)
+        logger.debug("Start obtaining information")
         bot.send_message(message.chat.id, content)
+        logger.debug("Message to a bot: " + content)
 
     elif (message.text.lower() == 'good morning'):
+        logger.debug("Message to change from good morning to goodmorning in a telegram text")
         bot.send_message(message.chat.id,
                          "Good morning flow for " + str(user_id))
+
     else:
         bot.send_message(message.chat.id, message.text)
-
-    # if (message.text == 'great'):
-    #         action_data = lib.run_action(actionline)
-    #         text = json.dumps(action_data)
-    #         bot.send_message(message.chat.id, text)
-    #     elif (message.text == "good night"):
-    #         bot.send_message( message.chat.id, "Good morning flow for " + str(user_id))
-    #     else:
-    #         bot.send_message(message.chat.id, message.text)
-
-    # person ="Alex"
-    # flow ="goodmorning"
-    # data = lib.get_flowdata(person, flow)
-    # print(data)
-    # if not data:
-    #     return  "Error = no data for this user and flow"
-    # simple_list = []
-    # for idx, line in enumerate(data):
-    #     actionline = line[0]
-
 
 bot.polling(none_stop=True)
