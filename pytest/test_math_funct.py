359c42dcaa7056a6b066640fd1857d7cdbf3646c
nepomnyashchiy <nepomnyashchii@utexas.edu>
2019-07-23 21:03:37 +0000

    wwww

--- pytest/test_math_funct.py
+++ pytest/test_math_funct.py
@@ -2,14 +2,16 @@ import math_func
 
 def test_add():
     """Test first function"""
-    assert math_func.add(7,3)==10
+    # assert math_func.add(7,3)==10
     assert math_func.add(7)==9
     assert math_func.add(5) == 7
-    return
 
 def test_product():
-    """Test second functio"""
+    """Test second function"""
     assert math_func.product(5,5)==25
-    assert math_func.product(7)==10
-    assert math_func.add(5) == 14
+    assert math_func.product(5)==10
+    assert math_func.product(7) == 14
+
+"""command: pytest test_math_funct_py"""
+"""more detailed command: pytest test_math_func.py"""
 
