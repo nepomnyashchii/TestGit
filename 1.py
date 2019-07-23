a24266396c66470e125517ac6420478cf3e77f9a
nepomnyashchiy <nepomnyashchii@utexas.edu>
2019-07-23 14:23:47 +0000

    www

--- 1.py
+++ 1.py
@@ -8,21 +8,25 @@ def name ():
 def age ():
     age = input("Enter: What is your age: ")
     my_age = "You are " + age + " years old" + "\n"
-    print(age)
+    print(my_age)
+    if int(age)> 16:
+        return car()
+    else:
+        sys.exit("You are too young")
 
 def car():
     car = input ("Enter: what model is your car: ")
     my_car = car
     print(my_car)
+    if my_car == "Mercedes":
+        print("It is my car")
+    else:
+        sys.exit("You are in a wrong car")
     return my_car
-    
+
 name()
 age()
 
-my_car = car()
-if my_car == "Mercedes":
-    print("Everything is good")
-else:
-    sys.exit("Wrong model")
+
 
 
