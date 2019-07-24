0cec801142760d49b604c1bc11bc08b8169d9b5e
nepomnyashchiy <nepomnyashchii@utexas.edu>
2019-07-24 01:15:29 +0000

    ddd

--- Final/Secret/curlrun.sh
+++ Final/Secret/curlrun.sh
@@ -3,7 +3,3 @@ curl -d '{
     "pin": 1234,
     "exp": 1000
 }' -H "Content-Type: application/json" -X POST http://localhost:5000/secret
-
-# todo/id POST Request (add asdasdasd inside the table in the column text): We use Body as a request (letter d is from there)
-# curl -d '{"text":"asdasdasd"}' -H "Content-Type: application/json" -X POST http://localhost:5000/todo
-# curl -d '{"key1":"value1", "key2":"value2"}' -H "Content-Type: application/json" -X POST http://localhost:3000/data
