2316e7254706abef823025057365a90aaaa5fd5c
nepomnyashchiy <nepomnyashchii@utexas.edu>
2019-07-24 03:15:31 +0000

    eeeddd

--- Final/Secret/integration_test.py
+++ Final/Secret/integration_test.py
@@ -13,6 +13,10 @@ response = requests.post(url = API_ENDPOINT, data=data)
 response_url = response.text
 print(response_url)
 
-response = requests.get("http://localhost:5000/secret/eb3474ac-003e-4dc3-a2f9-47f304b853c7/1234")
+sid = "69e90627-e82d-481a-9818-b5b7b63e33bd"
+msg = 1234
+URL = "http://localhost:5000/secret/sid/msg"
+
+response = requests.get("http://localhost:5000/secret/69e90627-e82d-481a-9818-b5b7b63e33bd/1234")
 response_url = response.text
 print(response_url)
