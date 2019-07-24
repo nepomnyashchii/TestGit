a7b60a93d70226228b164fbcd3b131e402651d64
nepomnyashchiy <nepomnyashchii@utexas.edu>
2019-07-24 01:15:16 +0000

    ddd

--- Final/Todo/curlrun.sh
+++ Final/Todo/curlrun.sh
@@ -11,13 +11,19 @@
 # curl -d '{"text":"asdasdasd"}' -H "Content-Type: application/json" -X POST http://localhost:5000/todo
 
 # # todo/id PUT Request (add from the dictionary text to text and to done:TRUE): We use Body as a request
-curl -d '{
-    "text":"asdasd",
-    "done":true
-}' -H "Content-Type: application/json" -X PUT http://localhost:5000/todo/64
+# curl -d '{
+#     "text":"asdasd",
+#     "done":true
+# }' -H "Content-Type: application/json" -X PUT http://localhost:5000/todo/64
 
 # #todo/id DELETE Request (delete from the dictionary the data with certain id)
-curl -H "Content-Type: application/json" -X DELETE http://localhost:5000/todo/64
+# curl -H "Content-Type: application/json" -X DELETE http://localhost:5000/todo/64
+
+curl -d '{
+    "msg":"success",
+    "pin": 1234,
+    "exp": 1000
+}' -H "Content-Type: application/json" -X POST http://localhost:5000/secret
 
 
 
