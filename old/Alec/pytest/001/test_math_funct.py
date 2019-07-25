b3d34083f917a789a10cd5aff7f002d13c73c589
nepomnyashchiy <nepomnyashchii@utexas.edu>
2019-07-25 13:21:08 +0000

    ee

--- old/Alec/pytest/001/test_math_funct.py
+++ old/Alec/pytest/001/test_math_funct.py
@@ -5,6 +5,7 @@ import pytest
 def test_add():
     """Test first function"""
     assert math_func.add(7,3) == 10
+    """assert function providing desired result"""
     # assert math_func.add(7,3)==9 for testing errors
     assert math_func.add(7) == 9
     assert math_func.add(5) == 7
