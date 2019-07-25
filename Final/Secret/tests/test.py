c80c96d4f72afe266af08951f4064b39896f11d2
nepomnyashchiy <nepomnyashchii@utexas.edu>
2019-07-25 17:27:21 +0000

    fff

--- Final/Secret/tests/test.py
+++ Final/Secret/tests/test.py
@@ -15,11 +15,11 @@ payload = json.dumps(data)
 test_1_response = requests.post(url, data=payload, headers=headers)
 test_1 = test_1_response.json()
 print(test_1)
+
 if test_1 is None and "sid" not in test_1.keys():
-    test_1_response == False
     sys.exit("test_1 integration test failed")
 else:
-    test_1_response ==True
+    test_1 == True
     result = test_1
     sid = result["sid"]
     print(sid)
@@ -27,12 +27,11 @@ else:
 
 pin = data["pin"]
 print(pin)
-test_2_response = requests.get("http://localhost:5000/secret/" + sid + "/" + str(pin))
 
+test_2_response = requests.get("http://localhost:5000/secret/" + sid + "/" + str(pin))
 test_2 = test_2_response.json()
-if test_1_response == True:
+if test_1:
     if test_2 is None and "msg" not in test_2.keys():
-        test_2_response == False
         sys.exit("test_2 integration test failed")
     else:
         test_2_response == True
@@ -47,14 +46,13 @@ data = {
 payload = json.dumps(data)
 test_3_response = requests.delete(url, data=payload, headers=headers)
 test_3 = test_3_response.json()
-if test_2_response == True:
-    if test_3 is None and "deleted_id" not in test_3.keys():
-        test_3_response == False
+print(test_3)
+if test_2:
+    if test_3 is None and "deleted_sid" not in test_3.keys():
         sys.exit("test_3 integration test failed")
     else:
-        test_3_response == True
         result = test_3
-        deleted_id = result["deleted_id"]
+        deleted_id = result["deleted_sid"]
         print(deleted_id)
 
 sys.exit("All integration tests are done")
