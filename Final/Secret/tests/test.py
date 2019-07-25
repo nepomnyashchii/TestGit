438e9e8b59a8a3177f166e0731c7658a02d6b5cb
nepomnyashchiy <nepomnyashchii@utexas.edu>
2019-07-25 16:50:03 +0000

    hhh

--- Final/Secret/tests/test.py
+++ Final/Secret/tests/test.py
@@ -1,10 +1,9 @@
 import requests
 import json
+import sys
 
 url = "http://localhost:5000/secret"
 headers = {'Content-Type': "application/json"}
-
-# payload = '{"msg":"ggg", "pin": 12345,"exp": 10000}'
 
 data = {'msg': 'success',
         'pin': 1234,
@@ -12,56 +11,50 @@ data = {'msg': 'success',
 
 payload = json.dumps(data)
 
-response = requests.post(url, data=payload, headers=headers)
-
-if response.json() is None:
-    print("There is no information from the post_request")
+test_1_response = requests.post(url, data=payload, headers=headers)
+test_1 = test_1_response.json()
+print(test_1)
+if test_1 is None and "sid" not in test_1.keys() or "pin" not in test_1.keys():
+    test_1_response == False
+    # sys.exit("test_1 integration test failed")
 else:
-    response.json()
-    print("Information for post_method succesfully received")
-    print(response.json())
+    test_1_response ==True
+    result = test_1
+    sid = result["sid"]
+    pin = data["pin"]
+    print(sid)
+    print(pin)
 
 
-result = response.json()
+test_2_response = requests.get("http://localhost:5000/secret/" + sid + "/" + str(pin))
 
-sid = result["sid"]
-pin = data["pin"]
-
-print(sid)
-print(pin)
-
-URL = "http://localhost:5000/secret/" + sid + "/" + str(pin)
-
-response = requests.get(URL)
-
-if response.json() is None:
-    print("There is no information from the get_request")
-else:
-    response.json()
-    print("Information for post_method succesfully received")
-    print(response.json())
-
-result = response.json()
-msg = result["msg"]
-print(msg)
+test_2 = test_2_response.json()
+if test_1_response == True:
+    if test_2 is None and "msg" not in test_2.keys():
+        test_2_response == False
+        sys.exit("test_2 integration test failed")
+    else:
+        test_2_response == True
+        result = test_2
+        msg = result["msg"]
+        print(msg)
 
 data = {
     "sid": sid,
     "pin": pin}
 
 payload = json.dumps(data)
-response = requests.delete(url, data=payload, headers=headers)
+test_3_response = requests.delete(url, data=payload, headers=headers)
+test_3 = test_3_response.json()
+if test_2_response == True:
+    if test_3 is None and "deleted_id" not in test_3.keys():
+        test_3_response == False
+        sys.exit("test_3 integration test failed")
+    else:
+        test_3_response == True
+        result = test_3
+        deleted_id = result["deleted_id"]
+        print(deleted_id)
 
-if response.json() is None:
-    print("There is no information from the delete_request")
-else:
-    response.json()
-    print("Information for post_method succesfully received")
-    print(response.json())
-
-result = response.json()
-deleted_id = result["deleted_id"]
-
-
-# print(response.json())
+sys.exit("All integration tests are done")
 
