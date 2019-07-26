1e9368859582bcc6a07d35f3b115b7728398d88f
nepomnyashchiy <nepomnyashchii@utexas.edu>
2019-07-26 00:14:24 +0000

    aaa

--- old/telegrambot/001_copy.py
+++ old/telegrambot/001_copy.py
@@ -8,14 +8,18 @@ bot = telebot.TeleBot("873802609:AAGrF4tID-CD4lufzgMjqNEnqLxhCaEweLM")
 def send_echo(message):
     bot.send_message(message.chat.id, message.text + '?', reply_markup=keyboard())
 
-def keyboard(btn2=None):
-	markup = types.ReplyKeyboardMarkup(one_time_keyboard=True, resize_keyboard=True)
-	btn1 = types.KeyboardButton('people')
-	btn2 = types.KeyboardButton('water')
-	markup.add(btn1)
-	markup.add(btn2)
-	return markup
 
-reply_markup=keyboard()
+def keyboard(btn2=None):
+    markup = types.ReplyKeyboardMarkup(one_time_keyboard=True, resize_keyboard=True)
+    btn1 = types.KeyboardButton('people')
+    btn2 = types.KeyboardButton('water')
+    btn3 = types.KeyboardButton("milk")
+    markup.add(btn1)
+    markup.add(btn2)
+    markup.add(btn3)
+    return markup
+
+
+reply_markup = keyboard()
 
 bot.polling(none_stop=True)
