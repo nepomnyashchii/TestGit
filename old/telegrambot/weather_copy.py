c6376ed6249f51d99262ada65d540d4c96400802
nepomnyashchiy <nepomnyashchii@utexas.edu>
2019-07-26 00:19:02 +0000

    rr

--- old/telegrambot/weather_copy.py
+++ old/telegrambot/weather_copy.py
@@ -22,14 +22,16 @@ def send_echo(message):
     else:
         return "No such city"
 
-def keyboard():
-	markup = types.ReplyKeyboardMarkup(one_time_keyboard=True, resize_keyboard=True)
-	btn1 = types.KeyboardButton('Brooklyn')
-	markup.add(btn1)
-	return markup
+def keyboard(btn2=None):
+    markup = types.ReplyKeyboardMarkup(one_time_keyboard=True, resize_keyboard=True)
+    btn1 = types.KeyboardButton('Brooklyn')
+    btn2 = types.KeyboardButton('Moscow')
+    btn3 = types.KeyboardButton("Miami")
+    markup.add(btn1)
+    markup.add(btn2)
+    markup.add(btn3)
+    return markup
 
 reply_markup=keyboard()
-
-
 
 bot.polling (none_stop = True)
