2fd24bfcc635d26316a22f33cb0ea9c1b4e3a8b9
nepomnyashchiy <nepomnyashchii@utexas.edu>
2019-07-26 01:24:20 +0000

    e

--- old/telegrambot/001_copy.py
+++ old/telegrambot/001_copy.py
@@ -1,13 +1,13 @@
 import telebot
 from telebot import types
 
+
 bot = telebot.TeleBot("873802609:AAGrF4tID-CD4lufzgMjqNEnqLxhCaEweLM")
 
 
 @bot.message_handler(content_types=['text'])
 def send_echo(message):
     bot.send_message(message.chat.id, message.text + '?', reply_markup=keyboard())
-
 
 def keyboard(btn2=None):
     markup = types.ReplyKeyboardMarkup(one_time_keyboard=True, resize_keyboard=True)
@@ -18,7 +18,6 @@ def keyboard(btn2=None):
     markup.add(btn2)
     markup.add(btn3)
     return markup
-
 
 reply_markup = keyboard()
 
