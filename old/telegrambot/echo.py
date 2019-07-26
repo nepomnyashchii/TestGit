58e93cd98b40e11005fdc67a822a659a66b9b923
nepomnyashchiy <nepomnyashchii@utexas.edu>
2019-07-26 13:40:09 +0000

    dddd

--- old/telegrambot/echo.py
+++ old/telegrambot/echo.py
@@ -7,7 +7,8 @@ bot = telebot.TeleBot("873802609:AAGrF4tID-CD4lufzgMjqNEnqLxhCaEweLM")
 
 @bot.message_handler(content_types=['text'])
 def send_echo(message):
-    bot.send_message(message.chat.id, message.text + '?', reply_markup=keyboard())
+     chat_id = message.chat.id
+     bot.send_message(chat_id, message.text, parse_mode='HTML',reply_markup=balance_key(chat_id))
 
 def keyboard(btn2=None):
     markup = types.ReplyKeyboardMarkup(one_time_keyboard=True, resize_keyboard=True)
@@ -18,6 +19,12 @@ def keyboard(btn2=None):
     markup.add(btn2)
     markup.add(btn3)
     return markup
+
+
+def balance_key(chat_id):
+	keyboard = types.InlineKeyboardMarkup()
+	keyboard.add(types.InlineKeyboardButton(text='Go to the main menu',callback_data="message.text"))
+	return keyboard
 
 reply_markup = keyboard()
 
