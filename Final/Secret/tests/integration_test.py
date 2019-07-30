58b8ee611e68916648a89bcddb732cbe2fcf921d
Alexander Yakovis <coolspammail@gmail.com>
2019-07-30 18:30:33 +0000

    exit status

--- Final/Secret/tests/integration_test.py
+++ Final/Secret/tests/integration_test.py
@@ -17,7 +17,7 @@ test_1_response = requests.post(url, data=payload, headers=headers)
 test_1_json = test_1_response.json()
 
 if test_1_json is None or (test_1_json is not None and "sid" not in test_1_json.keys()):
-    sys.exit("Test 1 failed")
+    sys.exit("Test 1 failed", 1)
 print("Test 1 passed")
 
 # Prepare Values for tests
@@ -30,9 +30,8 @@ print("Starting Test 2")
 test_2_response = requests.get(url + "/" + sid + "/" + str(pin))
 test_2_json = test_2_response.json()
 if test_2_json is None or (test_2_json is not None and "msg" not in test_2_json.keys()):
-    sys.exit("Test 2 failed")
+    sys.exit("Test 2 failed", 2)
 print("Test 2 passed")
-
 
 
 # Prepare Values for tests
@@ -45,7 +44,7 @@ test_3_response = requests.delete(url, data=payload, headers=headers)
 test_3_json = test_3_response.json()
 
 if test_3_json is None or (test_3_json is not None and "deleted_sid" not in test_3_json.keys()):
-    sys.exit("Test 3 failed")
+    sys.exit("Test 3 failed", 3)
 deleted_id = test_3_json["deleted_sid"]
 
 print("Test 3 passed \n\n\n")
