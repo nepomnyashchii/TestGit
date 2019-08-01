bdcaee4cff77d2cae9bd1ad75c0ee43689058bb4
nepomnyashchiy <nepomnyashchii@utexas.edu>
2019-08-01 19:02:34 +0000

    qq

--- Final/Secret/tests/curlrun.sh
+++ Final/Secret/tests/curlrun.sh
@@ -5,9 +5,15 @@ curl -d '{
     "exp": 1000
 }' -H "Content-Type: application/json" -X POST http://localhost:5000/secret
 
-# pin = 1234
-# sid = 1012e8c1-8fe7-491a-9050-554b115ce46a
+pin = 1234
+sid = 83c30c7d-3571-4dbb-b64e-e888d12d7300
 
-#  curl -H "Content-Type: application/json" -X GET http://localhost:5000/secret/sid/pin
+ curl -H "Content-Type: application/json" -X GET http://localhost:5000/secret/sid/pin
 
-#  curl -H "Content-Type: application/json" -X DELETE http://localhost:5000/secret/sid/pin
+ curl -H "Content-Type: application/json" -X DELETE http://localhost:5000/secret/sid/pin
+
+curl - d '{
+    "pin": 1234,
+    "sid" : "83c30c7d-3571-4dbb-b64e-e888d12d7300"
+}'
+-H "Content-Type: application/json" -X DELETE http://localhost:5000/secret/sid/pin
