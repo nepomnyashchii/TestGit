794023dd2659bdebf83c706a92726a879b042e4c
nepomnyashchii <nepomnyashchii@gmail.com>
2019-08-02 19:38:20 +0000

    jjjkk

--- excell/pandas1more.py
+++ excell/pandas1more.py
@@ -1,11 +1,11 @@
 import sys
 import pandas as pd
 
-data = pd.read_excel('./data.xlsx', sheet_name='SalesOrders', column_index=0)
+data = pd.read_excel('./data.xlsx', sheet_name='SalesOrders')
 data2 = data.drop(columns = ["OrderDate"])
 print(data2)
 data3=data2.to_csv(index=False)
 # print(data)
-# print(data3)
+print(data3)
 
 
