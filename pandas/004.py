b228f4e4fdc7029bf98fb84af5762d723796d17e
nepomnyashchii <nepomnyashchii@gmail.com>
2019-08-02 15:14:29 +0000

    dd

--- pandas/004.py
+++ pandas/004.py
@@ -5,7 +5,8 @@ data = {
     'oranges': [0, 3, 7, 2]
 }
 
-purchases = pd.DataFrame(data)
+purchases = pd.DataFrame(data, index=["John", "Alex", "Mary", "Mike"])
+purchases2 = purchases.loc["John"]
 # purchases.index = ["row1", "row2", "row3", "row4"]
 print(purchases)
-
+print(purchases2)
