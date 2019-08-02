b763bd3f4f40311ddeb095865c2e6b98175dcece
nepomnyashchii <nepomnyashchii@gmail.com>
2019-08-02 14:58:14 +0000

    aaa

---
+++ pandas/003.py
@@ -0,0 +1,11 @@
+import pandas as pd
+
+data = {
+    'apples': [3, 2, 0, 1],
+    'oranges': [0, 3, 7, 2]
+}
+
+purchases = pd.DataFrame(data)
+purchases.index = ["row1", "row2", "row3", "row4"]
+print(purchases)
+
