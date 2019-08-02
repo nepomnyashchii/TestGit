60d8b4e0d9b43b474e7e195a78d61f1f78e65f89
nepomnyashchii <nepomnyashchii@gmail.com>
2019-08-02 22:06:56 +0000

    ss

--- excell/pandas2.py
+++ excell/pandas2.py
@@ -2,10 +2,8 @@ import sys
 import pandas as pd
 
 data = pd.read_excel('./data.xlsx', sheet_name='SalesOrders')
-data2 = data.drop(columns = ["OrderDate"])
+data2 = data.drop(columns=["OrderDate"])
 print(data2)
-data3=data2.to_csv(index=False)
+data3 = data2.to_csv(index=False)
 # print(data)
 print(data3)
-
-
---
+++ excell/pandas3.py
@@ -0,0 +1,35 @@
+import mysql.connector
+import pandas as pd
+
+mydb = mysql.connector.connect(
+    host="db4free.net",
+    user="coolspammail",
+    passwd="coolspammail-pass",
+    database="coolspammail")
+
+sql = "SELECT * FROM secret"
+mycursor = mydb.cursor()
+
+myresult = mycursor.fetchall()
+
+df =pd.DataFrame(myresult)
+
+data3 = df.to_csv(index=False)
+print(df)
+print(data3)
+
+
+
+# mycursor.execute('SELECT * FROM table_name')
+
+# table_rows = db_cursor.fetchall()
+
+# df = pd.DataFrame(table_rows)
+
+# sql = "SELECT msg, created, exp FROM secret WHERE id =  %s AND pin = %s"
+# params = (sid, int(pin))
+# mycursor.execute(sql, params)
+# myresult = mycursor.fetchone()
+# close_db(mydb)
+# return_value = myresult
+# logger.debug("Value from DB returned" + str(return_value))
