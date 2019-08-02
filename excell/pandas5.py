f6e924a939d3d74fc5c06749f091ac918a76b45f
nepomnyashchii <nepomnyashchii@gmail.com>
2019-08-02 23:08:43 +0000

    kkl

--- excell/pandas5.py
+++ excell/pandas5.py
@@ -20,19 +20,6 @@ df = pd.DataFrame(myresult)
 data3 = df.to_json()
 print(df)
 print(data3)
-# print(data4)
 
 
-# mycursor.execute('SELECT * FROM table_name')
 
-# table_rows = db_cursor.fetchall()
-
-# df = pd.DataFrame(table_rows)
-
-# sql = "SELECT msg, created, exp FROM secret WHERE id =  %s AND pin = %s"
-# params = (sid, int(pin))
-# mycursor.execute(sql, params)
-# myresult = mycursor.fetchone()
-# close_db(mydb)
-# return_value = myresult
-# logger.debug("Value from DB returned" + str(return_value))
