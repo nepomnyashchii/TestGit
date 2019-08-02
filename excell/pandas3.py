6626e73cb95516fe15fd24b392e1f5281e470932
nepomnyashchii <nepomnyashchii@gmail.com>
2019-08-02 22:25:36 +0000

    sss

--- excell/pandas3.py
+++ excell/pandas3.py
@@ -1,5 +1,6 @@
 import mysql.connector
 import pandas as pd
+import openpyxl
 
 mydb = mysql.connector.connect(
     host="db4free.net",
@@ -8,15 +9,21 @@ mydb = mysql.connector.connect(
     database="coolspammail")
 
 sql = "SELECT * FROM secret"
+
 mycursor = mydb.cursor()
+
+mycursor.execute(sql)
 
 myresult = mycursor.fetchall()
 
 df =pd.DataFrame(myresult)
 
 data3 = df.to_csv(index=False)
+data4= df.to_excel(r'File Name.xlsx', index=False)
+
 print(df)
 print(data3)
+# print(data4)
 
 
 
