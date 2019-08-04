8b49a7f15b23278605c6b9430c1637a2131b8bb8
nepomnyashchii <nepomnyashchii@gmail.com>
2019-08-04 14:54:46 +0000

    SSS

--- excell/pandas6.py
+++ excell/pandas6.py
@@ -2,7 +2,7 @@ import pandas as pd
 import mysql.connector
 
 
-  INSERT INTO xxx (OrderDate,Region,Rep,Item,Units,Unit Cost,Total)
+  """INSERT INTO xxx (OrderDate,Region,Rep,Item,Units,Unit Cost,Total)
          VALUES ('%s', '%s', '%s', '%s', %s, %s, %s);"""
 
 data = pd.read_excel('./data.xlsx', sheet_name='SalesOrders')
@@ -22,8 +22,8 @@ test_query = '''DROP TABLE IF EXISTS "test_pandas"
 
                 CREATE TABLE Order
                 (
-                    OrderDate NULL,
-                    Region NULL,
+                    OrderDate, VARCHAR(20)
+                    Region,
                     Rep,
                     Item,
                     Units,
@@ -31,7 +31,7 @@ test_query = '''DROP TABLE IF EXISTS "test_pandas"
                     Total,
                 )
 
-                INSERT INTO test_pandas (id,transaction_dt,units,measures)
+                INSERT INTO test_pandas (OrderData, Region, Rep, item, Units, Unit Cost, Total)
                 VALUES (%s,%s,%s,%s) '''
 
 mycursor = mydb.cursor()
