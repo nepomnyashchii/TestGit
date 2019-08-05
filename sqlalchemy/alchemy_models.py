2d06d0ec96d42785f4b2b1dde218d530a0d09d15
nepomnyashchii <nepomnyashchii@gmail.com>
2019-08-05 14:42:56 +0000

    aaa

---
+++ sqlalchemy/alchemy_models.py
@@ -0,0 +1,42 @@
+from sqlalchemy.ext declrative import declarative_base
+from sqlalchemy import (Column, Integer, String, ForeignKey, create_engine)
+from sqlalchemy import (Column, Integer, String, ForeignKey, create_engine)
+from sqlalchemy.orm import relationship
+
+
+engine = create_engine('sqlite://:memory:')
+Base = declarative_base()
+
+Class Person(Base):
+    __tablename__ = "person"
+
+    id = Column(Integer, primary_key=True)
+    name = column(String)
+    age = Column(Integer)
+
+    addresses = relationship(
+        "Address",
+        'back_populates'='person',
+        'order_by'='Address.email',
+        'cascade'='all, delete-orphan'
+    )
+
+    def __repr__ (self):
+        return f'{self.name}(id = {self.id})'
+
+class Address (Base):
+    __tablename__ = 'address'
+
+    id = Column(Integer, primary_key = True)
+    email = Column(String)
+    person_id = Column(ForeignKey('person.id'))
+    person = relationship ("Person", back_populates = 'addresses')
+
+    def __str__ (self):
+        return self.email
+
+        __repr__ = __str__
+
+Base.metadata.create_all (engine)
+
+
