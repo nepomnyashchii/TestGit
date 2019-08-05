0715cb4f4e21591ba51f09f95ba7b6120a844eec
nepomnyashchii <nepomnyashchii@gmail.com>
2019-08-05 02:15:51 +0000

    dd

---
+++ excell/pandas4.py
@@ -0,0 +1,11 @@
+import pandas as pd
+import mysql.connector
+from sqlalchemy.engine import create_engine
+
+engine = create_engine(
+    'mysql+mysqlconnector://coolspammail:coolspammail-pass@db4free.net/coolspammail')
+
+df = pd.read_sql_table('test_pandas2', engine)
+
+
+print(df)
