87c7caa6f61df42e9aca64d7fd47969b5cf6fa26
nepomnyashchii <nepomnyashchii@gmail.com>
2019-08-05 18:34:21 +0000

    sss

--- old/telegrambot/weather.py
+++ old/telegrambot/weather.py
@@ -11,12 +11,17 @@ def send_weather(message):
 
     API_key = '1bdcae6b7d23f180361c8878a965c9f8'
     owm = pyowm.OWM(API_key)
-    observation = owm.weather_at_place(message.text)
-    w = observation.get_weather()
-    temperature = w.get_temperature('celsius')['temp']
-    answer = "In " + message.text + " temperature now is: " + str(temperature) + " degrees celcius" + "\n"
-    answer +="In our city " + w.get_detailed_status()
-    return bot.send_message(message.chat.id, answer, reply_markup=keyboard())
+    try:
+        observation = owm.weather_at_place(message.text)
+        w = observation.get_weather()
+        temperature = w.get_temperature('celsius')['temp']
+        answer = "In " + message.text + " temperature now is: " + str(temperature) + " degrees celcius" + "\n"
+        answer +="In our city " + w.get_detailed_status()
+        return bot.send_message(message.chat.id, answer, reply_markup=keyboard())
+    except Exception as error:
+        print("Such city does not exist in the openweather library")
+        bot.send_message(message.chat.id, "Such city does not exist in the openweather library")
+
 
 
 def keyboard(btn2=None):
