a01451ad61590a8dd3f7eb8eafe5e624621ba25d
nepomnyashchii <nepomnyashchii@gmail.com>
2019-08-06 23:46:19 +0000

    aaaa

--- Final/Flows/server.py
+++ Final/Flows/server.py
@@ -55,6 +55,47 @@ def run(username, flow):
         data=simple_list
     )
 
+
+# ------------------------------
+# -------- GET USERNAME FLOW ----------
+# ------------------------------
+
+@app.route('/run/<string:username>')
+
+def run_user(username):
+    logger.debug('Run invoked')
+    flow = "goodmorning"
+    data = lib.get_flowdata(username, flow)
+    print(data)
+    # data = [(u'weather:Brooklyn, NY',), (u'news:3',), (u'norris:3',), (u'news:2',), (u'thecocktail:random',), (u'thecocktail:random',)]
+    logger.debug("Data from DB(actionline):" + str(data))
+    if not data:  # len(data) == 0:
+        return jsonify(
+            error="no data for this user and flow",
+            username=username,
+            flow=flow,
+            time=datetime.datetime.now()
+        )
+
+    simple_list = []
+    for idx, line in enumerate(data):
+        actionline = line[0]
+        logger.debug("Actionline: " + str(actionline))
+        action_data = lib.run_action(actionline)
+        simple_list.append({
+            "action": idx,
+            "type": actionline.split(":")[0],
+            "data": action_data
+        })
+
+    logger.debug("Action data" + str(action_data))
+    return jsonify(
+        username=username,
+        flow=flow,
+        time=datetime.datetime.now(),
+        data=simple_list
+    )
+
 # ------------------------------
 # -------- errorhandler  -------
 # ------------------------------
