4df50a9ae425848374e052adca77ca41185e6cf8
nepomnyashchii <nepomnyashchii@gmail.com>
2019-08-07 16:06:18 +0000

    ggg

--- Final/NoSQLFlows/tests/integration_test.py
+++ Final/NoSQLFlows/tests/integration_test.py
@@ -7,9 +7,10 @@ url = "http://localhost:5000/run"
 
 # Prepare Values for tests
 username = "Alec" or "Alex"
+flow = "goodmorning"
 # Test 1
 print("Starting Test")
-test_1_response = requests.get(url + "/" + username)
+test_1_response = requests.get(url + "/" + username + "/" + flow)
 test_1_json = test_1_response.json()
 print(test_1_json)
 if test_1_json is None or "data" not in test_1_json.keys():
