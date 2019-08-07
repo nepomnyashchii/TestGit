53c37534a37cd769b75f2d0e11e8a1caead12eec
Alexander Yakovis <coolspammail@gmail.com>
2019-08-07 16:30:41 +0000

    fix test 1

--- Final/NoSQLFlows/tests/integration_test.py
+++ Final/NoSQLFlows/tests/integration_test.py
@@ -6,14 +6,20 @@ import sys
 base_url = "http://localhost:5000/run"
 
 # Prepare Values for tests
-username = "Alec" # or "Alex"
+username = "Alex"
 flow = "goodmorning"
 # Test 1
 print("Starting Test")
 url = base_url + "/" + username + "/" + flow
 print(url)
-test_1_response = requests.get()
-test_1_json = test_1_response.json()
+test_1_response = requests.get(url)
+
+try:
+    test_1_json = test_1_response.json()
+    # ...
+except ValueError:
+     sys.exit("Test 1 failed")
+
 print(test_1_json)
 if test_1_json is None or "data" not in test_1_json.keys():
     sys.exit("Test failed", 1)
