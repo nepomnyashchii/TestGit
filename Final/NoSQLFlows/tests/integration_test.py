3bc9f3cae82a37ba6fcc83ae98109dae97ac8409
Alexander Yakovis <coolspammail@gmail.com>
2019-08-07 17:26:58 +0000

    test

--- Final/NoSQLFlows/tests/integration_test.py
+++ Final/NoSQLFlows/tests/integration_test.py
@@ -1,28 +1,56 @@
 import requests
 import json
 import sys
-
-# Prepare Values for test
 base_url = "http://localhost:5000/run"
 
-# Prepare Values for tests
-username = "Alex"
-flow = "goodmorning"
-# Test 1
-print("Starting Test")
-url = base_url + "/" + username + "/" + flow
-print(url)
-test_1_response = requests.get(url)
 
+# Prepare Values for test 1
+username = "alex"
+flow = "goodmorning"
+url = base_url + "/" + username + "/" + flow
+
+print("Starting Test 1")
+test_1_response = requests.get(url)
+if (test_1_response.status_code != 200):
+    sys.exit("Test 1 failed status = " + str(test_1_response.status_code))
 try:
     test_1_json = test_1_response.json()
-    # ...
 except ValueError:
-     sys.exit("Test 1 failed")
+    sys.exit("Test 1 failed no json in response")
 
-print(test_1_json)
-if test_1_json is None or "data" not in test_1_json.keys():
-    sys.exit("Test failed", 1)
-print("Test passed")
+if (test_1_json is None) or ("data" not in test_1_json.keys()):
+    sys.exit("Test failed bed response data")
+print("Test 1 Success")
+
+
+
+
+# Prepare Values for test 2
+username = "Fake user Name"
+flow = "goodmorning"
+url = base_url + "/" + username + "/" + flow
+
+print("Starting Test 2")
+test_2_response = requests.get(url)
+if (test_2_response.status_code == 404):
+    print("Test 2 Success")
+else:
+    sys.exit("Test 2 failed status = " + str(test_2_response.status_code))
+
+
+
+
+# Prepare Values for test 3
+username = "alex"
+flow = "Fake Flow Name"
+url = base_url + "/" + username + "/" + flow
+
+print("Starting Test 3")
+test_3_response = requests.get(url)
+if (test_3_response.status_code == 404):
+    print("Test 3 Success")
+else:
+    sys.exit("Test 3 failed status = " + str(test_3_response.status_code))
+
 
 sys.exit("integration test is done\n\n\n")
