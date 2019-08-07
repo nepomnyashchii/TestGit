4aa2e4d8d69b882af69a49072dc7bd1d31c9cc7f
nepomnyashchii <nepomnyashchii@gmail.com>
2019-08-07 13:07:44 +0000

    ww

--- Final/Flows/tests/curlrun.sh
+++ Final/Flows/tests/curlrun.sh
@@ -1,7 +1,14 @@
 
- username = Alex
+# Run to obtain information for news, jokes, cocktail, temperature in a city
+# through username
+
+username = Alex
  curl -H "Content-Type: application/json" -X GET http://localhost:5000/run/username/
 
+
+
+# Run to obtain information for news, jokes, cocktail, temperature in a city
+# through username and flow
 username = Alex
 flow = goodmorning
 curl -H "Content-Type: application/json" -X GET http://localhost:5000/run/username/flow
