bdb8e7a804c5298e29783293a50eddaedbaf7e12
nepomnyashchii <nepomnyashchii@gmail.com>
2019-08-07 03:42:31 +0000

    aaa

--- Final/Flows/tests/integration_test.py
+++ Final/Flows/tests/integration_test.py
@@ -6,15 +6,14 @@ import sys
 url = "http://localhost:5000/run"
 
 # Prepare Values for tests
-
 username = "Alec" or "Alex"
 # Test 1
 print("Starting Test")
 test_1_response = requests.get(url + "/" + username)
 test_1_json = test_1_response.json()
 print(test_1_json)
-if test_1_json is None or "error" or "status" in test_1_json.keys():
-    sys.exit("Test failed", 2)
+if test_1_json is None or "data" not in test_1_json.keys():
+    sys.exit("Test failed", 1)
 print("Test passed")
 
 sys.exit("integration test is done\n\n\n")
