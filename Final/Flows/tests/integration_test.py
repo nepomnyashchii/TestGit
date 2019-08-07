cdfc2c7476e86fe6b252a01d6a87eb9321b9b4c6
Alexander Yakovis <coolspammail@gmail.com>
2019-08-07 17:49:47 +0000

    asdcopy tests

--- Final/Flows/tests/integration_test.py
+++ Final/Flows/tests/integration_test.py
@@ -1,19 +1,55 @@
 import requests
 import json
 import sys
+from colorama import Fore, Style
+base_url = "http://localhost:5000/run"
 
-# Prepare Values for test
-url = "http://localhost:5000/run"
 
-# Prepare Values for tests
-username = "Alec" or "Alex"
-# Test 1
-print("Starting Test")
-test_1_response = requests.get(url + "/" + username)
-test_1_json = test_1_response.json()
-print(test_1_json)
-if test_1_json is None or "data" not in test_1_json.keys():
-    sys.exit("Test failed", 1)
-print("Test passed")
+print(Fore.YELLOW + "Test flows APIs " + Style.RESET_ALL)
+
+# Prepare Values for test 1
+username = "alex"
+flow = "goodmorning"
+url = base_url + "/" + username + "/" + flow
+
+print("Starting Test 1")
+test_1_response = requests.get(url)
+if (test_1_response.status_code != 200):
+    sys.exit("Test 1 failed status = " + str(test_1_response.status_code))
+try:
+    test_1_json = test_1_response.json()
+except ValueError:
+    sys.exit("Test 1 failed no json in response")
+
+if (test_1_json is None) or ("data" not in test_1_json.keys()):
+    sys.exit("Test failed bed response data")
+print(Fore.GREEN + "Test 1 Success" + Style.RESET_ALL)
+
+
+# Prepare Values for test 2
+username = "Fake user Name"
+flow = "goodmorning"
+url = base_url + "/" + username + "/" + flow
+
+print("Starting Test 2")
+test_2_response = requests.get(url)
+if (test_2_response.status_code == 404):
+    print(Fore.GREEN + "Test 2 Success" + Style.RESET_ALL)
+else:
+    sys.exit("Test 2 failed status = " + str(test_2_response.status_code))
+
+
+# Prepare Values for test 3
+username = "alex"
+flow = "Fake Flow Name"
+url = base_url + "/" + username + "/" + flow
+
+print("Starting Test 3")
+test_3_response = requests.get(url)
+if (test_3_response.status_code == 404):
+    print(Fore.GREEN + "Test 3 Success" + Style.RESET_ALL)
+else:
+    sys.exit("Test 3 failed status = " + str(test_3_response.status_code))
+
 
 sys.exit("integration test is done\n\n\n")
