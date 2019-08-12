8f1ffd0ab49f6be9708865c5becc0213c73791ee
nepomnyashchii <nepomnyashchii@gmail.com>
2019-08-12 22:27:34 +0000

    jj

--- exercise/008.py
+++ exercise/008.py
@@ -1,10 +1,10 @@
-numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9] # Declaring the tuple
+numbers = [1, 2, 3, 4, 5] # Declaring the tuple
 odd_number = 0
 even_number = 0
 for x in numbers:
-        if not x % 2:
-    	     even_number+=1
-        else:
-    	     odd_number+=1
+    if not x % 2:
+        even_number+=1
+    else:
+        odd_number+=1
 print(even_number)
 print(odd_number)
