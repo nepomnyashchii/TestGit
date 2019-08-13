44cd43f4162517d26bad31841880cc754b7f816d
nepomnyashchii <nepomnyashchii@gmail.com>
2019-08-13 23:08:37 +0000

    ffff

---
+++ oldtraining/exercise/017.py
@@ -0,0 +1,13 @@
+# def reverse_slicing(s):
+#     return s[::-1]
+# print(reverse_slicing("alec"))
+
+def reverse_for_loop(s):
+    s1 = ''
+    for c in s:
+        s1 = c + s1
+        print(s1)  # appending chars in reverse order
+    return s1
+
+print(reverse_for_loop("alec"))
+
