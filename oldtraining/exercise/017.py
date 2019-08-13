0e0777c069dee575c58ba9d4c1dc6fb49446f330
nepomnyashchii <nepomnyashchii@gmail.com>
2019-08-13 23:09:50 +0000

    ddd

--- oldtraining/exercise/017.py
+++ oldtraining/exercise/017.py
@@ -2,12 +2,14 @@
 #     return s[::-1]
 # print(reverse_slicing("alec"))
 
+
 def reverse_for_loop(s):
     s1 = ''
     for c in s:
+        print(c)
         s1 = c + s1
         print(s1)  # appending chars in reverse order
     return s1
 
-print(reverse_for_loop("alec"))
 
+print(reverse_for_loop("alec"))
