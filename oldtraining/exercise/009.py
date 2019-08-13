21cbdd0b6ac1a63168f7364d86b1eeec3da5f008
nepomnyashchii <nepomnyashchii@gmail.com>
2019-08-13 17:49:22 +0000

    aaa

---
+++ oldtraining/exercise/009.py
@@ -0,0 +1,5 @@
+for i in range(6):
+    if (i == 3):
+        continue
+    print(i,end=' ')
+print("\n")
---
+++ oldtraining/exercise/011.py
@@ -0,0 +1,15 @@
+for fizzbuzz in range(50):
+    print(fizzbuzz)
+    if fizzbuzz % 3 == 0 and fizzbuzz % 5 == 0:
+        print("fizzbuzz")
+        continue
+    elif fizzbuzz % 3 == 0:
+        print("fizz")
+        continue
+    elif fizzbuzz % 5 == 0:
+        print("buzz")
+        continue
+
+
+# # a%=3
+# print(a)
