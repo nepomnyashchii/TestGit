68d2a72f37b79428165098946f50f1119db291d1
nepomnyashchiy <nepomnyashchii@utexas.edu>
2019-09-05 02:20:52 +0000

    111

--- assighnment/001.py
+++ assighnment/001.py
@@ -2,9 +2,10 @@ import pymongo
 import dns
 client = pymongo.MongoClient("mongodb+srv://nepomnyashchii:natasha1977#5@cluster0-6p7nv.mongodb.net/test?retryWrites=true&w=majority")
 db = client["test"]
-mycol = db["sasha"]
-result = {"names":
-        [
+mycol = db["sasha1"]
+result = {"names": "all",
+"list":
+[
             {
     "name": "alex",
     "phone": "234",
--- assighnment/002.py
+++ assighnment/002.py
@@ -2,6 +2,11 @@ import pymongo
 import dns
 client = pymongo.MongoClient("mongodb+srv://nepomnyashchii:natasha1977#5@cluster0-6p7nv.mongodb.net/test?retryWrites=true&w=majority")
 db = client["test"]
-mycol = db["sasha"]
-myquery = {"names": all}
+mycol = db["sasha1"]
+myquery = {"names": "all"}
+mydoc = mycol.find_one(myquery)
+myresult=mydoc
+print(myresult)
+
+
 
