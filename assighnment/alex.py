0eacff745e9be10b27e94ec7610139948438a3c6
Alexander Yakovis <coolspammail@gmail.com>
2019-09-05 20:01:54 +0000

    alex2

--- assighnment/alex.py
+++ assighnment/alex.py
@@ -4,12 +4,12 @@ client = pymongo.MongoClient(
     "mongodb+srv://nepomnyashchii:natasha1977#5@cluster0-6p7nv.mongodb.net/test?retryWrites=true&w=majority")
 db = client["test"]
 mycol = db["sasha3"]
-myquery = {"name": {"$regex": "alex"}}
+myquery = {"name": {"$regex": "a"}}
 mydocs = mycol.find(myquery)
 sql = ''
 for x in mydocs:
     sql += "INSERT INTO clients (name, phone, email) VALUES ('" + \
-        x["name"]+"', '" + x["phone"] + "', '" + x["email"] + "');"
+        x["name"]+"', '" + x["phone"] + "', '" + x["email"] + "');\n"
 print(sql)
 
 
@@ -21,6 +21,15 @@ mydb = mysql.connector.connect(
 )
 
 mycursor = mydb.cursor()
-mycursor.execute(sql, multi=True)
+
+mycursor.execute(sql, params=None, multi=True)
+
+# for i in mycursor.execute(sql, params=None, multi=True):
+#     print(i)
+
+# it = mycursor.execute(sql, params=None, multi=True)
+# for i in it:
+#     print(i)
+
 mydb.commit()
 print(mycursor.rowcount)
