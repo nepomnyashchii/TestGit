4a0fed9f450a806436045002d64a3ccc2917d3fa
nepomnyashchiy <nepomnyashchii@utexas.edu>
2019-09-06 16:42:33 +0000

    ggg

--- 004.py
+++ 004.py
@@ -15,3 +15,16 @@ a[2] = a[2] + 10
 print(a)
 
 "Questions between differences in put and post"
+
+x = {
+    "name": "alex",
+    "phone": "234",
+    "email" : "coolim@gmail.edu"}
+
+data = x["phone"]
+print(type(data))
+print(data)
+
+print(''"x["phone"]"')
+
+print(type('"+ x["phone"] + "'))
