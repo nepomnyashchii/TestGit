12c15ab5768387dc4d338774aee092cc44e7f725
nepomnyashchiy <nepomnyashchii@utexas.edu>
2019-09-11 18:13:15 +0000

    ddd

--- django/myfirst/setup/setup.py
+++ django/myfirst/setup/setup.py
@@ -1,2 +1,8 @@
 pip3 install django-grappelli
 pip3 install django
+
+
+# python3 manage.py runserver
+#python3 manage.py makemigrations articles
+#python3 manage.py sqlmigrate articles
+#python3 manage.py migrate
