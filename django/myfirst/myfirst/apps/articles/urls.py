37f1ac766b33373aa2eb523d308daeab3d8fc96c
nepomnyashchiy <nepomnyashchii@utexas.edu>
2019-09-11 18:05:19 +0000

    fff

--- django/myfirst/myfirst/apps/articles/urls.py
+++ django/myfirst/myfirst/apps/articles/urls.py
@@ -6,7 +6,7 @@ urlpatterns = [
     path('', views.index, name = 'index'),
     # path('test/', views.test, name = 'test')
     path('<int:article_id>/', views.detail, name = 'detail'),
-    path('<int:article_id>/leave_comment/', views.leave_comment, name = 'leave_comment'),
+    path('<int:article_id>/leave_comment/', views.detail, name = 'leave_comment'),
 ]
 
 # /articles/1
--- django/myfirst/myfirst/apps/articles/views.py
+++ django/myfirst/myfirst/apps/articles/views.py
@@ -2,10 +2,12 @@ from django.http import Http404, HttpResponseRedirect
 from django.http import HttpResponse
 from django.shortcuts import render
 
+from django.urls import reverse
+
 from .models import Article, Comment
 
-def index(request):
-    return HttpResponse("Hello World")
+# def index(request):
+#     return HttpResponse("Hello World")
 
 # """to run the program in gitpod use python3 manage.py runserver"""
 
@@ -22,7 +24,14 @@ def detail(request, article_id):
     except:
         raise Http404("Article is not found")
 
-    return render(request, 'articles/detail.html', {'article': a})
+    latest_comments_list = a.comment_set.order_by('-id')[:10]
+
+    return render(request, 'articles/detail.html', {'article': a, 'latest_comments_list': latest_comments_list})
 
 def leave_comment(request, article_id):
-    pass
+    try:
+        a = Article.objects.get(id = article_id)
+    except:
+        raise Http404("Article is not found")
+    a.comment_set.create(author_name= request.POST['name'], comment_text = request.POST['text'])
+    return HttpResponseRedirect( reverse('articles:detail', args = (a.id,)))
