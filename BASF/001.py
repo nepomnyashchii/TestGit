2a1442031ece98a590d278bdbafbe9fafc11ccef
nepomnyashchiy <nepomnyashchii@utexas.edu>
2019-10-03 03:09:29 +0000

    aaaa

---
+++ BASF/001.py
@@ -0,0 +1,31 @@
+apple  = [1,2,3]
+apple.reverse()
+print(apple)
+
+apple[2] = 200
+print(apple)
+
+apple.append(2000)
+print(apple)
+
+apple.append(30000)
+print(apple)
+
+apple.pop()
+print(apple)
+
+tup = (1,2,3)
+print(list(tup))
+print(tup)
+
+data = {"elk" : "apple"}
+
+print(data["elk"])
+
+"SELECT * From Something Where bla bla bla"
+
+data = []
+for a in data:
+    data.append(1)
+print(data)
+
