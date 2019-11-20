d69560f31701662e36d16755de6adaa9e4c58036
nepomnyashchiy <nepomnyashchii@utexas.edu>
2019-11-20 22:29:51 +0000

    commit

---
+++ Project/001.py
@@ -0,0 +1,13 @@
+# def compare (value_1, value_2):
+#     if value_1 == value_2:
+#         return "Yes"
+#     else:
+#         return "No"
+# print(compare(3,3))
+
+def compare(value_1, value_2, value_3):
+    if value_1== value_3 and value_2==value_3:
+        return "Yes"
+    else:
+        return "No"
+print (compare(1,1,1))
