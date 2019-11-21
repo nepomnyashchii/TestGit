714d932f5e6fe0cf072ee283583c84664b754a5b
nepomnyashchiy <nepomnyashchii@utexas.edu>
2019-11-21 22:31:38 +0000

    ee

--- Project/001.py
+++ Project/001.py
@@ -5,9 +5,108 @@
 #         return "No"
 # print(compare(3,3))
 
-def compare(value_1, value_2, value_3):
-    if value_1== value_3 and value_2==value_3:
+# def compare(value_1, value_2, value_3):
+#     if value_1== value_3 and value_2==value_3:
+#         return "Yes"
+#     else:
+#         return "No"
+# print (compare(1,1,1))
+
+
+# def compare (values):
+#     x = range(10)
+#     for l in x:
+#         if values==l: 
+#             return "Yes"
+#     else:
+#         return "No"
+# print (compare(20))
+
+
+# def compare(x1, x2, x3, x4, x5, x6, x7, x8, x9, x10):
+#     if x2==x1 and x3==x1 and x4==x1 and x5==x1 and x6==x1 and x7==x1 and x8==x1 and x9==x1 and x10==x1:
+#         return "Yes"
+#     else:
+#         return "No"
+# print compare(1,1,1,1,1,1,1,1,1,1)
+
+
+
+
+
+def compare(x1, x2, x3, x4, x5, x6, x7, x8, x9, x10):
+    if x2==x1 and x3==x1 and x4==x1 and x5==x1 and x6==x1 and x7==x1 and x8==x1 and x9==x1 and x10==x1 or x2 == None:
         return "Yes"
     else:
         return "No"
-print (compare(1,1,1))
+print (compare(1,1,1,1,1,1,1,1,1,1))
+
+
+# def compare(*args):
+#     for element in args:
+#         x1 = args[0]
+#         x2 = args[1]
+#         x3 = args[2]
+#         x4 = args[3]
+#         x5 = args[4]
+#         x6 = args[5]
+#         x7 = args[6]
+#         x8 = args[7]
+#         x9 = args[8]
+#         x10 = args[9]
+#         if x2==x1 and x3==x1 and x4==x1 and x5==x1 and x6==x1 and x7==x1 and x8==x1 and x9==x1 and x10==x1:
+#             return "Yes"
+#     else:
+#         return "No"
+# print (compare(1,1,1,1,1,1,1,1,1,1))
+
+# def compare(x1, x2, x3, x4, x5, x6, x7, x8, x9, x10):
+#     if x2==x1 and x3==x1 and x4==x1 and x5==x1 and x6==x1 and x7==x1 and x8==x1 and x9==x1 and x10==x1 or x2 == None:
+#         return "Yes"
+#     else:
+#         return "No"
+# print (compare(1,1,1,1,1,1,1,1,1,1))
+
+
+
+# return def compare (parameters)
+
+def compare(x1, x2, x3):
+    if x1 == x2 and x1 == x2 and x1 == x3:
+        return "Yes"
+    else:
+        return "No"
+compare (1,1,1)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# def my_sum(*args):
+#     result = 0
+#     # Iterating over the Python args tuple
+#     for x in args:
+#         result += x
+#     return result
+
+# print(my_sum(1, 2, 3))
+# 
+
+
+
+
+
+
+
+
+
+
