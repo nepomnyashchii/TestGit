f1d20c944d17e64a4ee2c4a2bd3d3bce6fa35021
nepomnyashchiy <nepomnyashchii@utexas.edu>
2019-11-26 21:45:18 +0000

    aaa

---
+++ javascript/004.js
@@ -0,0 +1,25 @@
+var a="John Doe"
+
+var a="John" + "    " + "Doe"
+console.log(a)
+'John Doe'
+
+var x, y;
+x = 5 + 6;
+y = x * 10;
+
+console.log(y)
+
+// Comments for this document;
+
+var lastname, lastName;
+lastName = "Doe";
+lastname = "Peterson";
+
+
+/*
+Comments for the multiple pages are shown here with the best one presented to solve this problem
+for the better future 
+*/
+
+console.log(lastname, lastName)
