053569eb812392534456636609ecba25e375d3e4
nepomnyashchiy <nepomnyashchii@utexas.edu>
2019-11-26 22:04:21 +0000

    wwwwww

---
+++ javascript/008.js
@@ -0,0 +1,10 @@
+
+var x = 16 + 4 + "Volvo";
+
+var y = "Volvo" + 16 + 4;
+
+console.log(x,y)
+
+
+
+
---
+++ javascript/009.js
@@ -0,0 +1,5 @@
+var y;           // Now x is undefined
+y = 5;           // Now x is a Number
+var x = "John";      // Now x is a String
+
+console.log(y,x)
