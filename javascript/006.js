e80a3b796794d17f7a71552d337d1081c41bf0e1
nepomnyashchiy <nepomnyashchii@utexas.edu>
2019-11-26 21:57:08 +0000

    sss

--- javascript/006.js
+++ javascript/006.js
@@ -1,5 +1,31 @@
-var pi = 3.14;
-var person = "John Doe";
-var answer = 'Yes I am!';
+// var pi = 3.14;
+// var person = "John Doe";
+// var answer = 'Yes I am!';
 
-console.log(pi, person, answer)
+// console.log(pi, person, answer)
+
+
+// carName = "Volvo";
+
+// console.log(carName)
+
+
+// var person = "John Doe",
+// carName = "Volvo",
+// price = 200;
+
+// console.log(person, carName, price)
+
+// var carName;
+
+// console.log(carName)
+
+var x = 2 + 3 + "5";
+
+// all the numbers will be added to each other as strings with an answer 55;
+
+console.log(x)
+
+
+
+
