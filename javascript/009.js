9b6d8aa3a95c5c873de80edfb76c2f5818a63ed2
nepomnyashchiy <nepomnyashchii@utexas.edu>
2019-11-27 16:11:26 +0000

    wwww

--- javascript/009.js
+++ javascript/009.js
@@ -3,3 +3,12 @@ y = 5;           // Now x is a Number
 var x = "John";      // Now x is a String
 
 console.log(y,x)
+
+
+var x = 5;
+var y = 5;
+var z = 6;
+(x == y)       // Returns true
+(x == z)       // Returns false
+
+console.log(x)
