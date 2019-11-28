037a378129c580017e57b01ea7c77967a6332662
nepomnyashchiy <nepomnyashchii@utexas.edu>
2019-11-28 17:44:43 +0000

    sss

---
+++ javascript/047.js
@@ -0,0 +1,6 @@
+var fruits = ["Banana", "Orange", "Apple", "Mango"];
+fruits[fruits.length] = "Kiwi";          // Appends "Kiwi" to fruits
+
+console.log(fruits)
+
+
---
+++ javascript/048.js
@@ -0,0 +1,28 @@
+var fruits = ["Banana", "Orange", "Apple", "Mango"];
+delete fruits[0];           // Changes the first element in fruits to undefined
+
+console.log(fruits)
+
+
+var fruits = ["Banana", "Orange", "Apple", "Mango"];
+fruits.splice(2, 0, "Lemon", "Kiwi");
+
+console.log(fruits)
+
+
+var fruits = ["Banana", "Orange", "Apple", "Mango"];
+fruits.splice(2, 2, "Lemon", "Kiwi");
+
+console.log(fruits)
+
+
+var fruits = ["Banana", "Orange", "Apple", "Mango"];
+fruits.splice(2, 1, "Lemon", "Kiwi");
+
+console.log(fruits)
+
+
+
+
+
+
