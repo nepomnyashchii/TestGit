f9ef80909aa0d75d44c8309befd19de99df6ce1e
nepomnyashchiy <nepomnyashchii@utexas.edu>
2019-11-28 16:55:40 +0000

    ssss

---
+++ javascript/043.js
@@ -0,0 +1,12 @@
+var fruits = ["Banana", "Orange", "Apple", "Mango"];
+var name = fruits.push("Lemon");    // adds a new element (Lemon) to fruits
+
+console.log(fruits)
+
+console.log(name)
+
+
+var fruits = ["Banana", "Orange", "Apple", "Mango"];
+fruits[6] = "Lemon"; 
+
+console.log(fruits)
---
+++ javascript/046.js
@@ -0,0 +1,29 @@
+var fruits = ["Banana", "Orange", "Apple", "Mango"];
+fruits.pop();              // Removes the last element ("Mango") from fruits
+
+console.log(fruits)
+
+var fruits = ["Banana", "Orange", "Apple", "Mango"];
+fruits.push("Kiwi"); 
+
+console.log(fruits)
+
+
+var fruits = ["Banana", "Orange", "Apple", "Mango"];
+fruits.shift();            // Removes the first element "Banana" from fruits
+
+console.log(fruits)
+
+
+var fruits = ["Banana", "Orange", "Apple", "Mango"];
+fruits.unshift("Lemon");    // Adds a new element "Lemon" to fruits
+
+console.log(fruits)
+
+
+
+
+
+
+
+
