e4a833946b8275eb2c2ce66ceaff78fa56804144
nepomnyashchiy <nepomnyashchii@utexas.edu>
2020-01-02 21:30:57 +0000

    aaa

--- mishpes/first/001.py
+++ mishpes/first/001.py
@@ -7,29 +7,30 @@
 # print(myFun(1,10,1))
 
 
-def myFun(*argv):
-    for arg in argv:
-        if arg!=argv[0]:
+# def myFun(*argv):
+#     for arg in argv:
+#         if arg!=argv[0]:
+#             return "No"
+#     return "Yes"
+# print(myFun(1,1,1))
+
+# par1= 1
+# par2 =2
+# par3 =3
+# par4= 4
+# par5 =5
+# par6 =6
+# par7 =7
+# par8 =8
+# par9 =9
+# par10 =10
+
+# print(data)
+
+def myFun(par1, par2, par3, par4, par5, par6, par7, par8, par9, par10):
+    data = [par1, par2, par3, par4, par5, par6, par7, par8, par9, par10]
+    for element in data:
+        if data[0]!= element:
             return "No"
     return "Yes"
-print(myFun(1,1,1))
-
-par1= 1
-par2 =2
-par3 =3
-par4= 4
-par5 =5
-par6 =6
-par7 =7
-par8 =8
-par9 =9
-par10 =10
-data = [par1, par2, par3, par4, par5, par6, par7, par8, par9, par10]
-
-print(data)
-def myFun(par1,par2,par3, par4, par5, par6, par7, par8, par9, par10):
-    for element in data:
-        if par1!=element:
-            return "No"
-print(myFun(1,1,2,1,1,1,1,1,1,1))
-
+print(myFun(1,2,1,1,1,1,1,1,1,1))
