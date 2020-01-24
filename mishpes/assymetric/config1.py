c7abf4ab08ee3c2f3ea1fb09a1938bff7a3e0e5c
nepomnyashchiy <nepomnyashchii@utexas.edu>
2020-01-24 12:22:40 +0000

    aaaa

---
+++ mishpes/assymetric/config1.py
@@ -0,0 +1,20 @@
+
+from cryptography.hazmat.backends import default_backend
+from cryptography.hazmat.primitives import serialization
+
+with open("private_key.pem", "rb") as key_file:
+    private_key = serialization.load_pem_private_key(
+        key_file.read(),
+        password=None,
+        backend=default_backend()
+    )
+
+with open("public_key.pem", "rb") as key_file:
+    public_key = serialization.load_pem_public_key(
+        key_file.read(),
+        backend=default_backend()
+    )
+
+print(private_key)
+print(public_key)
+
